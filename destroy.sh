ff854e9c3090cbf3d865c6a31da1df8273222409
Mikhail Volkhov <volhovm.cs@gmail.com>
2016-10-28 22:30:27 +0300

    Destroy confirmation

--- destroy.sh
+++ destroy.sh
@@ -1,2 +1,9 @@
+read -p "Are you sure you want to destroy everything ([y/n]): " -n 1 -r
+echo    
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+
 nixops destroy --all --confirm
 nixops delete --all --confirm
