7143f7d7b381d46ea62be3144ba473a4824436a3
Mothership Core <serokell.test@gmail.com>
2016-10-31 19:52:29 +0000

    Add autoDeploy script, modify destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -1,16 +1,9 @@
-read -p "Are you sure you want to destroy everything ([y/n]): " -n 1 -r
+read -p "Are you sure you want to destroy cardano deployment? ([y/n]): " -n 1 -r
 echo    
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
     exit 1
 fi
 
-read -p "LIKE REALLY EVERYTHING!!!" -n 1 -r
-echo    
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    exit 1
-fi
-
-nixops destroy --all --confirm
-nixops delete --all --confirm
+nixops destroy -d cardano --confirm
+nixops delete -d cardano --confirm
