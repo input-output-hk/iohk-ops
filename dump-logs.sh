9ad7d67d9dc12410d72e31f846486c5370be6db3
Cardano Deployer <serokell.test@gmail.com>
2016-11-05 21:05:56 +0000

    Add dump-logs.sh

---
+++ dump-logs.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+d=`date +%F_%H%M%S`
+logs_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/logs/$d
+mkdir -p "$logs_dir"
+cd "$logs_dir"
+
+function get_names {
+  nixops info --plain | awk '{print $1}' | grep node
+}
+
+for n in `get_names`; do
+  nixops scp --from $n /var/lib/cardano-node/cardano-node.log $n.log 1>&2 &
+done
+
+nixops info > layout.txt
+
+wait
+
+cd ..
+tar -czf $d.tgz $d
+
