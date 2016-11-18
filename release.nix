bcf94d1c13f162644365bdfaa914c82fc3f9b908
Cardano Deployer <serokell.test@gmail.com>
2016-11-18 19:49:08 +0000

    Add release.nix for ami generation

---
+++ release.nix
@@ -0,0 +1,20 @@
+{ pkgs ? (import <nixpkgs> {})}:
+
+with pkgs;
+
+rec {
+  image = (import <nixpkgs/nixos/lib/eval-config.nix> {
+    system = "x86_64-linux";
+    modules = [
+      (import ./nixops.nix).node1
+      <nixpkgs/nixos/maintainers/scripts/ec2/amazon-image.nix>
+    ];
+  }).config.system.build.amazonImage;
+
+  #node-ec2-image =
+  #  runCommand "node-ec2-image" { preferLocalBuild = true; } ''
+  #     mkdir -p $out/nix-support
+  #     xz -z -c ${image}/nixos.qcow2 > $out/node.img.xz
+  #     echo "file img $out/node.img.xz" > $out/nix-support/hydra-build-products
+  #  '';
+}
