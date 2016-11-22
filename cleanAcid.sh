a7bf37acc33e9b4016d2b877585aefc7a1404a8c
Cardano Deployer <serokell.test@gmail.com>
2016-11-22 10:45:54 +0000

    Removed useless script

--- cleanAcid.sh
+++
@@ -1,9 +0,0 @@
-
-echo "cleaning node0"
-nixops ssh node0-coordinator "systemctl stop cardano-node && rm -rf /var/lib/cardano-node/* && systemctl start cardano-node" && echo "done 0"&
-for i in {1..4}; do 
-	echo "cleaning node $i"
-	nixops ssh node$i "systemctl stop cardano-node && rm -rf /var/lib/cardano-node/* && systemctl start cardano-node" && echo "done $i"&
-done
-
-sleep 30
