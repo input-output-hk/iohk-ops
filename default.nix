772a8ccc6caff073bb176b5b739dba648ec84d18
Domen Kožar <domen@dev.si>
2016-11-28 18:08:48 +0100

    default.nix: simplify

--- default.nix
+++ default.nix
@@ -1,9 +1,6 @@
 { pkgs ? (import <nixpkgs> {}), ... }:
 
-# TODO share this parameters with nixops.nix
-let cconf = import ./compileconfig.nix;
-in
 (import ./srk-nixpkgs/default.nix { 
     inherit pkgs; 
-    inherit (cconf) genesisN slotDuration networkDiameter mpcRelayInterval;
+    inherit (import ./compileconfig.nix) genesisN slotDuration networkDiameter mpcRelayInterval;
 }).cardano-sl
