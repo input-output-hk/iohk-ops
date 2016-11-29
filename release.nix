9c239e8f6b9a37159ef87d9c9ed2ecf5c014fde5
Domen Kožar <domen@dev.si>
2016-11-29 12:25:28 +0100

    Use node0 for ami generation

--- release.nix
+++ release.nix
@@ -6,7 +6,7 @@ rec {
   image = (import <nixpkgs/nixos/lib/eval-config.nix> {
     system = "x86_64-linux";
     modules = [
-      (import ./deployments/cardano.nix).node1
+      (import ./deployments/cardano.nix).node0
       (import ./modules/amazon-image.nix)
       ({ ec2.hvm = true; })
     ];
