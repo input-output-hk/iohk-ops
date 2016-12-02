7f52feb15950a6a3bbb76f0f3f163fe53c9b5d83
Domen Kožar <domen@dev.si>
2016-12-02 15:14:38 +0100

    cardano jobset: skip building ami for now

--- jobsets/cardano.nix
+++ jobsets/cardano.nix
@@ -3,14 +3,14 @@
 with pkgs;
 
 rec {
-  image = (import <nixpkgs/nixos/lib/eval-config.nix> {
-    system = "x86_64-linux";
-    modules = [
-      (import ./deployments/cardano.nix).node0
-      (import ./modules/amazon-image.nix)
-      ({ ec2.hvm = true; })
-    ];
-  }).config.system.build.amazonImage;
+  #image = (import <nixpkgs/nixos/lib/eval-config.nix> {
+  #  system = "x86_64-linux";
+  #  modules = [
+  #    (import ./../deployments/cardano.nix).node0
+  #    (import ./../modules/amazon-image.nix)
+  #    ({ ec2.hvm = true; })
+  #  ];
+  #}).config.system.build.amazonImage;
 
   cardano-sl = import ./../default.nix;
 }
