136b50b02613098448b5176ddd51a9535a320005
Cardano Staging <staging@cardano>
2017-01-30 17:38:05 +0000

    Generate new amis

--- modules/amis.nix
+++ modules/amis.nix
@@ -1,7 +1,7 @@
 {
-  eu-central-1 = "ami-2be62044";
-  us-west-1 = "ami-32b8ef52";
-  ap-southeast-1 = "ami-53a00c30";
-  ap-southeast-2 = "ami-7a695119";
-  sa-east-1 = "ami-b7ba23db";
+  eu-central-1 = "ami-bd458bd2";
+  us-west-1 = "ami-fe35689e";
+  ap-southeast-1 = "ami-6469dc07";
+  ap-southeast-2 = "ami-0991966a";
+  sa-east-1 = "ami-602a4e0c";
 }
