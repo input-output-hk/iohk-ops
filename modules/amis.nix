dc66c09056775a6094d7fc0d6c5b0c1113b6fdd7
Cardano Deployer <cardano@deployer>
2017-02-19 12:51:20 +0000

    Update AMIs

--- modules/amis.nix
+++ modules/amis.nix
@@ -1,7 +1,7 @@
 {
-  eu-central-1 = "ami-bd458bd2";
-  us-west-1 = "ami-fe35689e";
-  ap-southeast-1 = "ami-6469dc07";
-  ap-southeast-2 = "ami-0991966a";
-  sa-east-1 = "ami-602a4e0c";
+  eu-central-1 = "ami-28dd1547";
+  us-west-1 = "ami-4d29752d";
+  ap-southeast-1 = "ami-3719ad54";
+  ap-southeast-2 = "ami-7ab9bf19";
+  sa-east-1 = "ami-dfd5b1b3";
 }
