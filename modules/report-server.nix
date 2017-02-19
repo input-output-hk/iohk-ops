6792a943398b1fe3c5d9d0dd473dcab77ccb33aa
Cardano Deployer <cardano@deployer>
2017-02-19 16:49:23 +0000

    add missing file

---
+++ modules/report-server.nix
@@ -0,0 +1,45 @@
+{ config, pkgs, lib, ... }:
+
+with (import ./../lib.nix);
+
+let
+  report-server = (import ./../srk-nixpkgs.nix { 
+    inherit pkgs;
+    inherit (cfg) genesisN slotDuration networkDiameter mpcRelayInterval;
+  }).hspkgs.cardano-report-server;
+  cfg = config.services.report-server;
+in {
+  options = {
+    services.report-server = {
+      enable = mkEnableOption "report-server";
+      port = mkOption { type = types.int; default = 5555; };
+      logsdir = mkOption { type = types.path; default = "/var/lib/report-server"; };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    users = {
+      users.report-server = {
+        group = "report-server";
+        home = cfg.logsdir;
+        createHome = true;
+      };
+      groups.report-server = {};
+    };
+
+    networking.firewall.allowedTCPPorts = [ cfg.port ];
+
+    systemd.services.report-server = {
+      description   = "";
+      after         = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        User = "report-server";
+        Group = "report-server";
+        ExecStart = ''
+          ${report-server}/bin/cardano-report-server -p ${toString cfg.port} --logsdir ${cfg.logsdir}
+        '';
+      };
+    };
+  };
+}
