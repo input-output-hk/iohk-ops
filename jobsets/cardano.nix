9fe5a522e940a6b5185b2a8ac48a03aecd4b090b
Domen Kožar <domen@dev.si>
2017-03-22 11:44:15 +0100

    fix: cannot refer to other paths

--- jobsets/cardano.nix
+++ jobsets/cardano.nix
@@ -3,14 +3,18 @@
 with pkgs;
 
 rec {
-  image = (import <nixpkgs/nixos/lib/eval-config.nix> {
-    system = "x86_64-linux";
-    modules = [
-      (import ./../deployments/cardano.nix).node0
-      (import ./../modules/amazon-image.nix)
-      ({ ec2.hvm = true; })
-    ];
-  }).config.system.build.amazonImage;
+  # TODO:
+  # in job ‘image’:
+  # The option `deployment' defined in `/nix/store/pp4mi8hmrk923kf18x0j88v35v0lx59z-git-export/modules/common.nix' does not exist.
+
+  #image = (import <nixpkgs/nixos/lib/eval-config.nix> {
+  #  system = "x86_64-linux";
+  #  modules = [
+  #    (import ./../deployments/cardano.nix).node0
+  #    (import ./../modules/amazon-image.nix)
+  #    ({ ec2.hvm = true; })
+  #  ];
+  #}).config.system.build.amazonImage;
 
   cardano-sl = import ./../default.nix;
 }
