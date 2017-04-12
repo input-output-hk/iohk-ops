1c0ee3909af359ba5ef6546ed6ca6418ecd4e0f9
Domen Kožar <domen@dev.si>
2017-04-12 15:40:39 +0200

    [DEVOPS-3] Setup datadog for prod nodes and hydra/deployer

---
+++ deployments/cardano-prod.nix
@@ -0,0 +1,12 @@
+with (import ./../lib.nix);
+
+let
+  conf = { config, pkgs, resources, ... }: {
+    imports = [
+      ./../modules/datadog.nix
+    ];
+    services.dd-agent.tags = ["prod"];
+  };
+in {
+  report-server = conf;
+} // (genAttrs' (range 0 13) (key: "node${toString key}") (name: conf))
