49785577a7a79112beecb02e0fb2e77940a1dcf8
Domen Kožar <domen@dev.si>
2017-04-19 15:33:26 +0200

    [DEVOPS-45] Monitor cardano-node process on datadog
    This requires upstream changes in nixpkgs.

--- deployments/cardano-prod.nix
+++ deployments/cardano-prod.nix
@@ -6,13 +6,24 @@ let
       ./../modules/datadog.nix
       ./../modules/papertrail.nix
     ];
-    services.dd-agent.tags = ["env:production"];
 
     # DEVOPS-64: disable log bursting
     services.journald.rateLimitBurst = 0;
 
     # Initial block is big enough to hold 3 months of transactions
     deployment.ec2.ebsInitialRootDiskSize = mkForce 700;
+
+    services.dd-agent.tags = ["env:production"];
+    services.dd-agent.processConfig = ''
+      init_config:
+
+      instances:
+      - name: cardano-node
+        search_string: ['cardano-node']
+        exact_match: False
+        thresholds:
+          critical: [1, 1]
+    '';
   };
 in {
   report-server = conf;
