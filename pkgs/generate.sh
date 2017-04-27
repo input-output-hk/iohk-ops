f631510b7262cc6ab7cb9e98b05802d9172ec235
Domen Kožar <domen@dev.si>
2017-04-27 17:58:34 +0200

    fix pkgs/generate.sh

--- pkgs/generate.sh
+++ pkgs/generate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env nix-shell
-#! nix-shell -j 4 -i bash -p pkgs.cabal2nix
+#! nix-shell -j 4 -i bash -p pkgs.cabal2nix pkgs.nix-prefetch-scripts
 #! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/464c79ea9f929d1237dbc2df878eedad91767a72.tar.gz
 
 set -xe
