e6f54d88aa0cbbd651a4b3e8f47cdfb338252f6a
Kosyrev Serge <serge.kosyrev@iohk.io>
2017-05-27 04:22:56 +0300

    [DEVOPS-24]  AWS.hs:  control S3 redirection of the Daedalus release bucket

---
+++ AWS.hs
@@ -0,0 +1,142 @@
+#!/usr/bin/env nix-shell
+#! nix-shell -j 4 -i runhaskell -p 'pkgs.haskellPackages.ghcWithPackages (hp: with hp; [ turtle ])'
+#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/f2c4af4e3bd9ecd4b1cf494270eae5fd3ca9e68c.tar.gz
+
+{-# LANGUAGE OverloadedStrings #-}
+
+import Control.Monad (forM_)
+import Data.Monoid ((<>))
+import Filesystem.Path.CurrentOS (encodeString)
+import Prelude hiding (FilePath)
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import Turtle
+
+data Command =
+  S3 S3
+  deriving (Show)
+
+data S3
+  = CheckDaedalusReleaseURLs
+  | SetDaedalusReleaseBuild RSpecOSX RSpecWin64
+  deriving (Show)
+
+data RSpecOSX   = OSX   BuildId      deriving (Show)
+data RSpecWin64 = Win64 AppveyorPath deriving (Show)
+
+data AppveyorPath = AppveyorPath T.Text deriving (Show)
+data BuildId      = BuildId      T.Text deriving (Show)
+data URL =
+  URL
+  { proto :: T.Text
+  , host  :: T.Text
+  , key   :: T.Text
+  } deriving (Show)
+
+ppURL:: URL -> T.Text
+ppURL (URL pro ho ke) = pro <> "://" <> ho <> "/" <> ke
+
+parser :: Parser Command
+parser =
+  subcommand "s3" "Control the S3-related AWS-ities."
+  (S3 <$> (subcommand "set-daedalus-release-build" "Set the S3 daedalus-<OS>-latest.<EXT> redirect to a particular version."
+            (SetDaedalusReleaseBuild
+             <$> (OSX   <$> (BuildId      <$> argText "build-id"      "Travis build id.  Example: '0.3.1526'"))
+             <*> (Win64 <$> (AppveyorPath <$> argText "appveyor-path" "AppVeyor build subpath.  Example: 'iw5m0firsneia7k2/artifacts/installers/daedalus-win64-0.3.1451.0'")))
+           <|>
+           subcommand "check-daedalus-release-urls" "Check the Daedalus release URLs." (pure CheckDaedalusReleaseURLs)))
+
+buildURL :: BuildId -> URL
+buildURL (BuildId id) =
+  URL "https" "s3.eu-central-1.amazonaws.com" $
+  mconcat ["daedalus-travis/Daedalus-installer-", id, ".pkg"]
+avpathURL :: AppveyorPath -> URL
+avpathURL (AppveyorPath avp) =
+  URL "https" "ci.appveyor.com" $
+  mconcat ["buildjobs/", avp, "-installer.exe"]
+
+show' :: Show a => a -> Text
+show' = T.pack . show
+
+main :: IO ()
+main = do
+  top <- options "Helper CLI around IOHK AWS functionality" parser
+  case top of
+    S3 s3 -> runS3 s3
+
+osxLive, w64Live :: URL
+osxLive = URL "http" "daedalus-installer-routing-rules.s3-website.eu-central-1.amazonaws.com" "daedalus-osx-latest.pkg"
+w64Live = URL "http" "daedalus-installer-routing-rules.s3-website.eu-central-1.amazonaws.com" "daedalus-win64-latest.exe"
+
+checkURL :: Text -> URL -> IO ()
+checkURL desc url = do
+  echo ""
+  printf ("============== Checking if "%s%" URL is live:") desc
+  echo ""
+  shells ("wget --tries=1 --spider " <> ppURL url) empty
+
+runS3 :: S3 -> IO ()
+runS3 CheckDaedalusReleaseURLs = do
+  checkURL "OSX release" osxLive
+  checkURL "W64 release" w64Live
+
+  echo "URLs live:"
+  echo ""
+  printf ("  OS X:   "%s%"\n") (ppURL osxLive)
+  printf ("  Win64:  "%s%"\n") (ppURL w64Live)
+
+runS3 (SetDaedalusReleaseBuild rsOSX@(OSX id) rsWin64@(Win64 avPath)) = do
+  let (osxurl@(URL osxpr osxcn osxpath), w64url@(URL w64pr w64cn w64path)) =
+        (buildURL id, avpathURL avPath)
+
+  echo $ unsafeTextToLine $ mconcat [ "Setting Daedalus release redirects to: "]
+  echo $ unsafeTextToLine $ mconcat [ "  OS X:   ", ppURL osxurl]
+  echo $ unsafeTextToLine $ mconcat [ "  Win64:  ", ppURL w64url]
+  checkURL "OSX build" osxurl
+  checkURL "W64 build" w64url
+
+  echo "Both URLs live, proceeding to update latest release build references to:"
+  echo $ unsafeTextToLine $ mconcat [ "  OS X:  ", ppURL osxurl]
+  echo $ unsafeTextToLine $ mconcat [ "  Win64: ", ppURL w64url]
+  echo ""
+
+  with (mktempfile "/tmp" "awstmp.json") $ \tmpfile -> do
+    writeTextFile tmpfile . T.unlines $
+      [ "{",
+        "    \"IndexDocument\": {",
+        "        \"Suffix\":                   \"index.html\"",
+        "    },",
+        "    \"RoutingRules\": [",
+        "        {",
+        "            \"Condition\": {",
+        "                \"KeyPrefixEquals\":  \"daedalus-osx-latest.pkg\"",
+        "            },",
+        "            \"Redirect\": {",
+        "                \"HttpRedirectCode\": \"303\",",
+        ["                \"Protocol\":         \"", osxpr, "\","] & mconcat,
+        ["                \"HostName\":         \"", osxcn, "\","] & mconcat,
+        ["                \"ReplaceKeyWith\":   \"", osxpath, "\""] & mconcat,
+        "            }",
+        "        },",
+        "        {",
+        "            \"Condition\": {",
+        "                \"KeyPrefixEquals\":  \"daedalus-win64-latest.exe\"",
+        "            },",
+        "            \"Redirect\": {",
+        "                \"HttpRedirectCode\": \"303\",",
+        ["                \"Protocol\":         \"", w64pr, "\","] & mconcat,
+        ["                \"HostName\":         \"", w64cn, "\","] & mconcat,
+        ["                \"ReplaceKeyWith\":   \"", w64path, "\""] & mconcat,
+        "            }",
+        "        }",
+        "    ]",
+        "}"]
+
+    -- shells ("cat " <> format fp tmpfile) empty
+    shells ("aws s3api put-bucket-website --bucket daedalus-installer-routing-rules --website-configuration file://" <> format fp tmpfile) empty
+    echo "Done.  Following URLs should be live within a minute:"
+    echo ""
+    printf ("  OS X:   "%s%"\n") (ppURL osxLive)
+    printf ("  Win64:  "%s%"\n") (ppURL w64Live)
+
+  pure ()
