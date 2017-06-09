3478c134ddb6d33041362e027c5e2d43ad864532
Domen Kožar <domen@dev.si>
2017-06-09 15:54:34 +0200

    Remove upload_keys.sh as it's now done by nixops

--- scripts/upload_keys.sh
+++
@@ -1,5 +0,0 @@
-for i in {0..13}; do
- nixops scp -d csl-testnet --to node$i ./nodes/key$((i+1)) /var/lib/cardano-node/key$((i+1)).sk
- nixops ssh -d csl-testnet node$i chown cardano-node:cardano-node /var/lib/cardano-node/key$((i+1)).sk
-done
-
