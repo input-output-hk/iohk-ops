4fec1905143b01a97a4d5f15bddf803bd0b48199
Domen Kožar <domen@dev.si>
2017-06-13 17:42:08 +0200

    datadog: fix staging config

--- deployments/cardano-nodes-env-staging.nix
+++ deployments/cardano-nodes-env-staging.nix
@@ -8,9 +8,10 @@ in {
     elasticIPs = mkNodeIPs nodes accessKeyId;
     datadogMonitors = (with (import ./../modules/datadog-monitors.nix); {
       cpu = mkMonitor (cpu_monitor // {
+        query = config: "avg(last_5m):avg:system.load.norm.1{host:${config.deployment.name}.machine} by {host} > 0.99";
         monitorOptions.thresholds = {
-	  warning = "0.95";
-	  critical = "0.99";
+          warning = "0.95";
+          critical = "0.99";
         };
       });
 
