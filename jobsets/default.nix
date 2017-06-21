962d8a505e72618f21c00ae1486239f1469a51b9
michael bishop <michael.bishop@iohk.io>
2017-06-21 15:14:26 -0300

    refactor jobsets/default.nix

--- jobsets/default.nix
+++ jobsets/default.nix
@@ -3,78 +3,50 @@
 # Followed by https://github.com/NixOS/hydra/pull/418/files
 
 let
+  iohkNixopsUri = "https://github.com/input-output-hk/iohk-nixops.git";
   pkgs = import nixpkgs {};
+  mkFetchGithub = value: {
+    inherit value;
+    type = "git";
+    emailresponsible = false;
+  };
+  nixpkgs-src = builtins.fromJSON (builtins.readFile ./../nixpkgs-src.json);
   defaultSettings = {
-    enabled = true;
+    enabled = 1;
     hidden = false;
-    input = "jobsets";
-    keep = 5;
-    shares = 42;
-    interval = 60;
+    nixexprinput = "jobsets";
+    keepnr = 5;
+    schedulingshares = 42;
+    checkinterval = 60;
     inputs = {
-      nixpkgs = {
-        type = "git";
-        value = "https://github.com/NixOS/nixpkgs.git b9628313300b7c9e4cc88b91b7c98dfe3cfd9fc4";
-      };
-      jobsets = {
-        type = "git";
-        value = "https://github.com/input-output-hk/iohk-nixops.git master";
-      };
+      nixpkgs = mkFetchGithub "https://github.com/NixOS/nixpkgs.git b9628313300b7c9e4cc88b91b7c98dfe3cfd9fc4";
+      jobsets = mkFetchGithub "${iohkNixopsUri} master";
     };
-    mail = false;
-    mailOverride = "";
+    enableemail = false;
+    emailoverride = "";
+  };
+  mkCardano = nixopsBranch: nixpkgsRev: {
+    nixexprpath = "jobsets/cardano.nix";
+    description = "Cardano SL";
+    inputs = {
+      nixpkgs = mkFetchGithub "https://github.com/NixOS/nixpkgs.git ${nixpkgsRev}";
+      jobsets = mkFetchGithub "${iohkNixopsUri} ${nixopsBranch}";
+    };
   };
   jobsetsAttrs = with pkgs.lib; mapAttrs (name: settings: defaultSettings // settings) (rec {
-    cardano-sl = {
-      path = "jobsets/cardano.nix";
-      description = "Cardano SL";
-    };
-    cardano-sl-staging = {
-      path = "jobsets/cardano.nix";
-      description = "Cardano SL";
-      inputs = {
-        nixpkgs = {
-          type = "git";
-          value = "https://github.com/NixOS/nixpkgs.git 7648f528de9917933bc104359c9a507c6622925c";
-        };
-        jobsets = {
-          type = "git";
-          value = "https://github.com/input-output-hk/iohk-nixops.git staging";
-        };
-      };
-    };
+    cardano-sl = mkCardano "master" "b9628313300b7c9e4cc88b91b7c98dfe3cfd9fc4";
+    cardano-sl-staging = mkCardano "staging" "7648f528de9917933bc104359c9a507c6622925c";
     deployments = {
-      path = "jobsets/deployments.nix";
+      nixexprpath = "jobsets/deployments.nix";
       description = "Builds for deployments";
     };
   });
+  jobsetJson = pkgs.writeText "spec.json" (builtins.toJSON jobsetsAttrs);
 in {
   jobsets = with pkgs.lib; pkgs.runCommand "spec.json" {} ''
     cat <<EOF
-    ${builtins.toXML declInput}
+    ${builtins.toJSON declInput}
     EOF
-    cat > $out <<EOF
-    {
-      ${concatStringsSep "," (mapAttrsToList (name: settings: ''
-        "${name}": {
-            "enabled": ${if settings.enabled then "1" else "0"},
-            "hidden": ${if settings.hidden then "true" else "false"},
-            "description": "${settings.description}",
-            "nixexprinput": "${settings.input}",
-            "nixexprpath": "${settings.path}",
-            "checkinterval": ${toString settings.interval},
-            "schedulingshares": ${toString settings.shares},
-            "enableemail": ${if settings.mail then "true" else "false"},
-            "emailoverride": "${settings.mailOverride}",
-            "keepnr": ${toString settings.keep},
-            "inputs": {
-              ${concatStringsSep "," (mapAttrsToList (inputName: inputSettings: ''
-                "${inputName}": { "type": "${inputSettings.type}", "value": "${inputSettings.value}", "emailresponsible": false }
-              '') settings.inputs)}
-            }
-        }
-      '') jobsetsAttrs)}
-    }
-    EOF
+    cp ${jobsetJson} $out
   '';
 }
