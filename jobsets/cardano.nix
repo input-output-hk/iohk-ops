1205b15d248fb09de113b63f3610e4224a32aa9b
michael bishop <michael.bishop@iohk.io>
2017-06-24 23:14:34 -0300

    setup the binary cache without using sudo and fix a merge conflict against master and make the path to nixpkgs-src.json relative to the script, with a local variable so it doesnt polute whatever script is sourcing it

--- jobsets/cardano.nix
+++ jobsets/cardano.nix
@@ -1,12 +1,10 @@
-{ pkgs' ? (import <nixpkgs> {}), supportedSystems ? [ "x86_64-linux" ] }:
+let
+  fixedNixpkgs = (import <nixpkgs> {}).fetchFromGitHub (builtins.fromJSON (builtins.readFile ../nixpkgs-src.json));
+in { pkgs ? (import fixedNixpkgs {}), supportedSystems ? [ "x86_64-linux" ] }:
 
 
 let
-  pkgs = if (builtins.getEnv "NIX_PATH_LOCKED" == "1") then
-    pkgs'
-  else
-    import (import ../lib.nix).fetchNixPkgs {};
-  iohkpkgs = import ./../default.nix {};
+  iohkpkgs = import ./../default.nix { inherit pkgs; };
 in with pkgs; rec {
   inherit (iohkpkgs) cardano-report-server-static cardano-sl-static cardano-sl-explorer-static cardano-sl;
   stack2nix = iohkpkgs.callPackage ./../pkgs/stack2nix.nix {};
--- scripts/set_nixpath.sh
+++ scripts/set_nixpath.sh
@@ -1,2 +1,6 @@
-export NIX_PATH=nixpkgs=https://github.com/NixOS/nixpkgs/archive/$(jq .rev < nixpkgs-src.json -r).tar.gz
-export NIX_PATH_LOCKED=1
+update_NIX_PATH() {
+  local scriptDir=$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")
+  export NIX_PATH=nixpkgs=https://github.com/NixOS/nixpkgs/archive/$(jq .rev < ${scriptDir}/../nixpkgs-src.json -r).tar.gz
+  export NIX_PATH_LOCKED=1
+}
+update_NIX_PATH
