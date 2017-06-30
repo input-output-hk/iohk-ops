c06bc33a5a299d0ea5b55dd1be064ea6be03f8ef
michael bishop <michael.bishop@iohk.io>
2017-06-30 18:41:33 -0300

    restore commits from staging branch in an attempt to fix all builds

--- pkgs/stack2nix.nix
+++ pkgs/stack2nix.nix
@@ -1,24 +1,24 @@
-{ mkDerivation, async, base, bytestring, Cabal, containers
-, data-fix, directory, fetchgit, filepath, Glob, hnix
-, monad-parallel, optparse-applicative, process, SafeSemaphore
-, stdenv, temporary, text, yaml
+{ mkDerivation, async, base, bytestring, containers, data-fix
+, directory, fetchgit, filepath, Glob, hnix, monad-parallel
+, optparse-applicative, process, SafeSemaphore, stdenv, temporary
+, text, yaml
 }:
 mkDerivation {
   pname = "stack2nix";
-  version = "0.1.2.0";
+  version = "0.1.0.0";
   src = fetchgit {
     url = "https://github.com/input-output-hk/stack2nix.git";
-    sha256 = "076rsasis3vxbfrp5r3aar4yz9qcrawy8hysvbix62823w12lfas";
-    rev = "3f2d4d31b1936dda71ce9a3be145c6407685045e";
+    sha256 = "01b8kc205m6p2q3l4an0gx9062w9szjkk139m3qnra7g4bh5551g";
+    rev = "59ee4de0223da8ad8ae56adb02f39ec365a20d42";
   };
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    async base bytestring Cabal containers data-fix directory filepath
-    Glob hnix monad-parallel process SafeSemaphore temporary text yaml
+    async base bytestring containers data-fix directory filepath Glob
+    hnix monad-parallel process SafeSemaphore temporary text yaml
   ];
-  executableHaskellDepends = [ base Cabal optparse-applicative ];
+  executableHaskellDepends = [ base optparse-applicative ];
   doCheck = false;
   description = "Convert stack.yaml files into Nix build instructions.";
-  license = stdenv.lib.licenses.mit;
+  license = stdenv.lib.licenses.bsd3;
 }
