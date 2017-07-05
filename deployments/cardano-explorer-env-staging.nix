f0667080efcb8eeb9fd6751839ddfb124fea9758
Domen Kožar <domen@dev.si>
2017-07-05 10:05:55 +0200

    [DEVOPS-188] Assign DNS to report-server and explorer

--- deployments/cardano-explorer-env-staging.nix
+++ deployments/cardano-explorer-env-staging.nix
@@ -1,10 +1,16 @@
 { accessKeyId, ... }:
 
 with (import ./../lib.nix);
-let
-  nodeStagingConf = import ./../modules/cardano-node-staging.nix;
-in {
-  sl-explorer = nodeStagingConf;
+
+{
+  sl-explorer = { config, ... }: {
+    imports = [ ./../modules/cardano-node-staging.nix ];
+
+    deployment.route53 = {
+      hostName = mkForce "cardano-explorer.aws.iohkdev.io";
+    };
+  };
+
   resources = {
     elasticIPs = {
       nodeip40 = { inherit region accessKeyId; };
