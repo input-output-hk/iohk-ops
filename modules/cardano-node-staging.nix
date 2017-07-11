a93a513c5c03f93d3bb91392d180fdd41241e242
Domen Kožar <domen@dev.si>
2017-07-11 13:48:01 +0200

    staging: rename config and add systemStart time

--- modules/cardano-node-staging.nix
+++ modules/cardano-node-staging.nix
@@ -15,6 +15,8 @@ with (import ./../lib.nix);
   deployment.route53.accessKeyId = config.deployment.ec2.accessKeyId;
   deployment.route53.hostName = "cardano-node-${toString config.services.cardano-node.testIndex}.aws.iohkdev.io";
 
+  services.cardano-node.systemStart = mkForce 1499433433;
+
   services.dd-agent.tags = ["env:staging"];
   services.dd-agent.processConfig = ''
     init_config:
