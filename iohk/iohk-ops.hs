ccecf6eed590029c00f6cf76cc5bfcdc38409187
Kosyrev Serge <serge.kosyrev@iohk.io>
2017-07-25 00:45:18 +0300

    [DEVOPS-56]  iohk-ops:  bump fake keygen in anticipation..
    ..actually, the node count is deducible from cluster.yaml, but..
    ..is the complication really worth it?

--- iohk/iohk-ops.hs
+++ iohk/iohk-ops.hs
@@ -294,6 +294,6 @@ runFakeKeys = do
   echo "Faking keys/key*.sk"
   testdir "keys"
     >>= flip unless (mkdir "keys")
-  forM_ (41:[1..14]) $
+  forM_ ([1..41]) $
     (\x-> do touch $ Turtle.fromText $ format ("keys/key"%d%".sk") x)
   echo "Minimum viable keyset complete."
