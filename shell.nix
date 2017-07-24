8386525775d71fc10a4e397ccaef765bdd6792b9
Kosyrev Serge <serge.kosyrev@iohk.io>
2017-07-24 20:35:16 +0300

    shell.nix:  add a couple of aliases

--- shell.nix
+++ shell.nix
@@ -56,6 +56,12 @@ mkDerivation {
     export NIX_PATH=nixpkgs=${nixpkgs}
     export NIX_PATH_LOCKED=1
     echo   NIX_PATH set to $NIX_PATH >&2
+
+    alias git-rev='git log -n1 --pretty=oneline'
+    echo "Aliases:"                                                                                                        >&2
+    echo                                                                                                                   >&2
+    echo "  git-rev:              git log -n1 --pretty=oneline"                                                            >&2
+    echo                                                                                                                   >&2
   '';
   license      = stdenv.lib.licenses.mit;
 };
