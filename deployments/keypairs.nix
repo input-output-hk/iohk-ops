55cc05a4ee8a8c722b34ce18122ede6526bb9d0b
Kosyrev Serge <serge.kosyrev@iohk.io>
2017-07-26 17:34:50 +0300

    [DEVOPS-139]  drop old firewall definitions

--- deployments/keypairs.nix
+++ deployments/keypairs.nix
@@ -1,40 +1,8 @@
 { accessKeyId, ... }:
 
 with (import ./../lib.nix);
-let
-  regions = [
-    "eu-central-1"
-    "eu-west-1"
-    "eu-west-2"
-    "ap-southeast-1"
-    "ap-southeast-2"
-    "ap-northeast-1"
-    "ap-northeast-2"
-  ];
-  genSG = sgs: mergeAttrs (map sgs regions);
-in {
+{
   resources = {
     ec2KeyPairs = ec2KeyPairs accessKeyId;
-    ec2SecurityGroups = genSG (region: {
-      /*"allow-cardano-${region}" = {
-        inherit region accessKeyId;
-        description = "Security group for cardano nodes";
-        rules = [{
-          fromPort = 22;
-          toPort = 22;
-          sourceIp = "0.0.0.0/0";
-        }];
-      };*/
-      "allow-all-${region}" = {
-        inherit region accessKeyId;
-        description = "Catch all";
-        rules = [{
-          protocol = "-1"; # All traffic
-          sourceIp = "0.0.0.0/0";
-          fromPort = 0;
-          toPort = 65535;
-        }];
-      };
-    });
   };
 }
