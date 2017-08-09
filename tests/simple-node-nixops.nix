e300c73d86b58b994637898854843651de876b13
Kosyrev Serge <serge.kosyrev@iohk.io>
2017-08-09 17:57:12 +0300

    Hydra tests:  update modules/cardano-node-config call sites to the node argument API changes

--- tests/simple-node-nixops.nix
+++ tests/simple-node-nixops.nix
@@ -1,8 +1,11 @@
+let
+  nodeArgs = import ./cardano-node-simple-config.nix;
+in
 import <nixpkgs/nixos/tests/make-test.nix> ({ pkgs, ... }: {
   name = "boot";
   nodes = {
     machine = { config, pkgs, ... }: {
-      imports = [ (import ../modules/cardano-node-config.nix 0 "") <nixops/nix/options.nix> <nixops/nix/resource.nix> ];
+      imports = [ (import ../modules/cardano-node-config.nix (nodeArgs.machine)) <nixops/nix/options.nix> <nixops/nix/resource.nix> ];
       services.cardano-node = {
         autoStart = true;
         initialKademliaPeers = [];
--- tests/simple-node.nix
+++ tests/simple-node.nix
@@ -1,8 +1,11 @@
+let
+  nodeArgs = import ./cardano-node-simple-config.nix;
+in
 import <nixpkgs/nixos/tests/make-test.nix> ({ pkgs, ... }: {
   name = "simple-node";
   nodes = {
     machine = { config, pkgs, ... }: {
-      imports = [ (import ../modules/cardano-node-config.nix 0 "") ];
+      imports = [ (import ../modules/cardano-node-config.nix (nodeArgs.machine)) ];
       virtualisation.qemu.options = [ "-cpu Haswell" ];
       services.cardano-node = {
         autoStart = true;
