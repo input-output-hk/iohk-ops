513f44ef6b41e191068381cf6ffeaf71371779db
Kosyrev Serge <serge.kosyrev@iohk.io>
2017-08-31 16:48:56 +0300

    lib.nix:  add 'us-west-1'

--- lib.nix
+++ lib.nix
@@ -76,6 +76,7 @@ in lib // (rec {
     cardano-test-ap-southeast-2 = { inherit accessKeyId; region = "ap-southeast-2"; };
     cardano-test-ap-northeast-1 = { inherit accessKeyId; region = "ap-northeast-1"; };
     cardano-test-ap-northeast-2 = { inherit accessKeyId; region = "ap-northeast-2"; };
+    cardano-test-us-west-1 = { inherit accessKeyId; region = "us-west-1"; };
   };
 
   volhovmKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDRMQ16PB/UvIEF+UIHfy66FNaBUWgviE2xuD5qoq/nXURBsHogGzv1ssdj1uaLdh7pZxmo/cRC+Y5f6dallIHHwdiKKOdRq1R/IWToMxnL/TTre+px6rxq21al9r4lvibelIU9vDn0R6OFZo+pRWyXUm33bQ4DVhwWiSls3Hw+9xRq4Pf2aWy//ey5CUTW+QkVdDIOFQG97kHDO3OdoNuaOMdeS+HBgH25bzSlcMw044T/NV9Cyi3y1eEBCoyqA9ba28GIl3vNADBdoQb5YYhBViFLaFsadzgWv5XWTpXV4Kwnq8ekmTcBkDzoTng/QOrDLsFMLo1nEMvhbFZopAfZ volhovm.cs@gmail.com";
