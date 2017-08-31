d49b0a2842204383c0eb2690f61a532c58ccc8de
Kosyrev Serge <serge.kosyrev@iohk.io>
2017-08-31 05:21:28 +0300

    default.nix:  define 'cardano-sl-tools-static'

--- default.nix
+++ default.nix
@@ -27,4 +27,7 @@ in {
                   --prefix PATH : "${pkgs.lib.makeBinPath iohk-ops-extra-runtime-deps}"
                 '';
              });
-} // cardano-sl-pkgs
+} // cardano-sl-pkgs // {
+  cardano-sl-tools-static      = justStaticExecutables cardano-sl-pkgs.cardano-sl-tools;
+  cardano-report-server-static = justStaticExecutables cardano-sl-pkgs.cardano-report-server;
+}
