0ac32b49cffcf9ac3b1d924121fe124c5d759dd4
Kosyrev Serge <serge.kosyrev@iohk.io>
2017-09-01 08:56:18 +0300

    DCONFIG:  testnet_staging_full

--- default.nix
+++ default.nix
@@ -5,7 +5,7 @@ in
 , config ? {}
 , pkgs ? (import (localLib.fetchNixPkgs) { inherit system config; })
 , compiler ? pkgs.haskell.packages.ghc802
-, dconfig ? "testnet_staging"
+, dconfig ? "testnet_staging_full"
 }:
 
 with pkgs.lib;
