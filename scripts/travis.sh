9ed5e762abaa161941449a11b7e8d047020bc4cd
Kosyrev Serge <serge.kosyrev@iohk.io>
2017-09-01 04:31:36 +0300

    travis.sh:  parametrise & add cleanups

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -3,25 +3,68 @@
 set -xe
 
 IOHK_OPS=${1:-iohk-ops}
+NIXOPS=${2:-nixops}
+WITH_CLEANUP=${3:-true}
+WITH_STAGING=${4:-true}
+WITH_PRODUCTION=${5:-true}
+WITH_DEVELOPMENT=${6:-true}
+WITH_EXPLORER=${7:-true}
+WITH_REPORT_SERVER=${8:-true}
+WITH_INFRA=${9:-true}
 
-nix-shell --run "./scripts/aws.hs --help"
-nixops --version
+# 0. Check all scripts compile
+# nixops --version
+# nix-shell --run "./scripts/aws.hs --help"
+# ${IOHK_OPS} --help
 
-# check all scripts compile
-${IOHK_OPS} --help
+# # 1. check all packages build
+# nix-instantiate jobsets/cardano.nix --show-trace
 
-# check all packages build
-nix-instantiate jobsets/cardano.nix --show-trace
+# 2. check all environments evaluate
+CLEANUP_DEPLS=""
+cleanup() {
+        set +xe
+        for depl in ${CLEANUP_DEPLS}
+        do
+                ${IOHK_OPS} --config ${depl}'.yaml' destroy delete >/dev/null 2>&1
+                rm -f                ${depl}'.yaml'
+        done
+}
+if test -n "${WITH_CLEANUP}"
+then trap cleanup EXIT
+fi
 
-# check deploy evaluations
-${IOHK_OPS} --testing template --environment production --topology 'topology-min.yaml' --config 'csl-production.yaml'       'csl-production'       Nodes Explorer ReportServer
-${IOHK_OPS} --testing template --environment staging    --topology 'topology-min.yaml' --config 'csl-staging.yaml'          'csl-staging'          Nodes Explorer ReportServer
-${IOHK_OPS} --testing template                          --topology 'topology-min.yaml' --config 'csl.yaml'                  'csl'                  Nodes Explorer ReportServer
-${IOHK_OPS} --testing template --environment staging    --topology 'topology-min.yaml' --config 'csl-explorer-staging.yaml' 'csl-explorer-staging'       Explorer
-${IOHK_OPS} --testing template --environment production --topology 'topology-min.yaml' --config 'inf.yaml'                  'inf'         Infra
-${IOHK_OPS}                                   --verbose --config 'csl-production.yaml'       create deploy --evaluate-only
-${IOHK_OPS}                                   --verbose --config 'csl-staging.yaml'          create deploy --evaluate-only
-${IOHK_OPS}                                   --verbose --config 'csl.yaml'                  create deploy --evaluate-only
-${IOHK_OPS}                                   --verbose --config 'csl-explorer-staging.yaml' create deploy --evaluate-only
-${IOHK_OPS}                                   --verbose --config 'tw.yaml'                   create deploy --evaluate-only
-${IOHK_OPS}                                   --verbose --config 'inf.yaml'                  create deploy --evaluate-only
+banner() {
+        echo -e "--\n--\n--  $*\n--\n--\n"
+}
+
+COMMON_OPTIONS="--nixops ${NIXOPS} --deployer-ip 0.0.0.0 --topology topology-min.yaml"
+CARDANO_COMPONENTS="Nodes ${WITH_EXPLORER:+Explorer} ${WITH_REPORT_SERVER:+ReportServer}"
+
+if test -n "${WITH_STAGING}"; then
+CLEANUP_DEPLS+=' test-stag'
+${IOHK_OPS} template  --config 'test-stag.yaml'   --environment staging    ${COMMON_OPTIONS} 'test-stag'    ${CARDANO_COMPONENTS}
+${IOHK_OPS} --verbose --config 'test-stag.yaml'   create deploy --evaluate-only
+banner 'Staging env evaluated'
+fi
+
+if test -n "${WITH_PRODUCTION}"; then
+CLEANUP_DEPLS+=' test-prod'
+${IOHK_OPS} template  --config 'test-prod.yaml'   --environment production ${COMMON_OPTIONS} 'test-prod'    ${CARDANO_COMPONENTS}
+${IOHK_OPS} --verbose --config 'test-prod.yaml'   create deploy --evaluate-only
+banner 'Production env evaluated'
+fi
+
+if test -n "${WITH_DEVELOPMENT}"; then
+CLEANUP_DEPLS+=' test-devo'
+${IOHK_OPS} template  --config 'test-devo.yaml'                            ${COMMON_OPTIONS} 'test-devo'    ${CARDANO_COMPONENTS}
+${IOHK_OPS} --verbose --config 'test-devo.yaml'   create deploy --evaluate-only
+banner 'Development env evaluated'
+fi
+
+if test -n "${WITH_INFRA}"; then
+CLEANUP_DEPLS+=' test-infra'
+${IOHK_OPS} template  --config 'test-infra.yaml'  --environment production ${COMMON_OPTIONS} 'test-infra'   Infra
+${IOHK_OPS} --verbose --config 'test-infra.yaml'  create deploy --evaluate-only
+banner 'Infra evaluated'
+fi
