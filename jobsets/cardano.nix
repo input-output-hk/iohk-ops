7d826e42bd161925f189cec0eab2de0370fb66c6
Domen Kožar <domen@dev.si>
2017-09-07 12:50:47 +0200

    [DEVOPS-302] Use upstream cardano jobset

--- jobsets/cardano.nix
+++ jobsets/cardano.nix
@@ -1,16 +1,17 @@
 let
   fixedNixpkgs = (import ../lib.nix).fetchNixPkgs;
-in {
-  pkgs ? (import fixedNixpkgs {}), supportedSystems ? [ "x86_64-linux" "x86_64-darwin" ]
-  , scrubJobs ? true
-}:
+in { pkgs ? (import fixedNixpkgs {})
+   , supportedSystems ? [ "x86_64-linux" "x86_64-darwin" ]
+   , scrubJobs ? true
+   }:
 
 with import (fixedNixpkgs + "/pkgs/top-level/release-lib.nix") { inherit supportedSystems scrubJobs; packageSet = import ../.; };
 
 let
   iohkpkgs = import ./../default.nix { inherit pkgs; };
   jobs = mapTestOn (packagePlatforms iohkpkgs);
-in with pkgs; rec {
-  inherit (jobs) cardano-report-server-static cardano-sl-static cardano-sl-explorer-static cardano-sl cardano-sl-tools-static iohk-ops stack2nix;
+  cardanoSrc = pkgs.fetchgit (builtins.fromJSON (builtins.readFile ./../cardano-sl-src.json));
+in rec {
+  inherit (jobs) iohk-ops;
   tests          = import ./../tests     { inherit pkgs; supportedSystems = [ "x86_64-linux" ]; };
-}
+} // (import "${cardanoSrc}/release.nix" {})
