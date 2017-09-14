fce318a92c6b3905813b5ad088d89d2b8e5cfd69
Kosyrev Serge <serge.kosyrev@iohk.io>
2017-09-14 04:11:36 +0300

    nodes-env-development:  allow-all SG

--- deployments/cardano-nodes-env-development.nix
+++ deployments/cardano-nodes-env-development.nix
@@ -6,5 +6,17 @@
 // {
   network.description = "Cardano Development";
 
-  resources.elasticIPs = nodesElasticIPs globals.nodeMap;
+  resources.ec2SecurityGroups =
+    listToAttrs (flip map globals.orgXRegions
+                 ({ org, region }:
+                  nameValuePair "allow-all-${org}-${region}"
+                                { inherit region;
+                                  accessKeyId = globals.orgAccessKeys.${org};
+                                  description = "Allow all ${org}/${region}";
+                                  rules = [{
+                                    protocol = "-1"; # Any
+                                    sourceIp = "0.0.0.0/0";
+                                    fromPort = 0; toPort = 65535;
+                                  }];
+                                }));
 }
