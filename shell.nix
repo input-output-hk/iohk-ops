609dee694da7943608bd17434fd1ea8020b06238
Kosyrev Serge <serge.kosyrev@iohk.io>
2017-10-11 15:40:52 +0300

    shell.nix:  document the runtime deps

--- shell.nix
+++ shell.nix
@@ -4,7 +4,9 @@
 
 let
   extraDeps = with pkgs;
-    [ wget awscli nodejs # for scripts/aws.hs
+    [ wget awscli  # for scripts/aws.hs
+      nodejs       # for cardano-sl/scripts/js/genesis-hash.js
+                   #     ..which also needs `npm install blakejs canonical-json`
     ];
   drv = pkgs.haskell.lib.overrideCabal
          (import ./default.nix {}).iohk-ops
