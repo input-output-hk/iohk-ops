9a0b5a3f14cdec8172a22a160ba7efb9f2a82454
Kosyrev Serge <serge.kosyrev@iohk.io>
2017-10-11 15:29:05 +0300

    shell.nix:  define the `io` alias to refer to an internally-defined `iohk-ops`

--- shell.nix
+++ shell.nix
@@ -1,5 +1,6 @@
 { localLib ? import ./lib.nix
 , pkgs ? import (localLib.fetchNixPkgs) {}
+, io-dev-mode ? false
 }:
 
 let
@@ -8,7 +9,15 @@ let
       nodejs       # for cardano-sl/scripts/js/genesis-hash.js
                    #     ..which also needs `npm install blakejs canonical-json`
     ];
-  drv = pkgs.haskell.lib.overrideCabal
-         (import ./default.nix {}).iohk-ops
-         (drv: { libraryHaskellDepends = extraDeps; });
-in drv.env
+  iohk-ops = pkgs.haskell.lib.overrideCabal
+             (import ./default.nix {}).iohk-ops
+             (drv: { libraryHaskellDepends = extraDeps; });
+  ioAlias = if io-dev-mode
+            then ''runhaskell -iiohk iohk/iohk-ops.hs''
+            else ''${iohk-ops}/bin/iohk-ops'';
+in pkgs.lib.overrideDerivation iohk-ops.env
+   (old: {
+     shellHook = ''
+       alias io='${ioAlias}'
+     '';
+    })
