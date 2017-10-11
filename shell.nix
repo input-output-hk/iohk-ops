f6322436e82637ce830f4cea8f723471b1899a17
Kosyrev Serge <serge.kosyrev@iohk.io>
2017-10-11 16:17:15 +0300

    default.nix:  absorb/merge runtime deps from shell.nix

--- shell.nix
+++ shell.nix
@@ -4,17 +4,10 @@
 }:
 
 let
-  extraDeps = with pkgs;
-    [ wget awscli  # for scripts/aws.hs
-      nodejs       # for cardano-sl/scripts/js/genesis-hash.js
-                   #     ..which also needs `npm install blakejs canonical-json`
-    ];
-  iohk-ops = pkgs.haskell.lib.overrideCabal
-             (import ./default.nix {}).iohk-ops
-             (drv: { libraryHaskellDepends = extraDeps; });
-  ioAlias = if io-dev-mode
-            then ''runhaskell -iiohk iohk/iohk-ops.hs''
-            else ''${iohk-ops}/bin/iohk-ops'';
+  iohk-ops = (import ./default.nix {}).iohk-ops;
+  ioAlias  = if io-dev-mode
+             then ''runhaskell -iiohk iohk/iohk-ops.hs''
+             else ''${iohk-ops}/bin/iohk-ops'';
 in pkgs.lib.overrideDerivation iohk-ops.env
    (old: {
      shellHook = ''
