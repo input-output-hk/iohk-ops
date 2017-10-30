2e8ab269e821523db21bb25dbd3017053a2d7fe4
Kosyrev Serge <serge.kosyrev@iohk.io>
2017-10-30 17:30:24 +0300

    [DEVOPS-479]  Regenerate iohk/default.nix

--- iohk/default.nix
+++ iohk/default.nix
@@ -1,6 +1,7 @@
 { mkDerivation, aeson, aeson-pretty, amazonka, amazonka-s3, base
-, bytestring, cassava, containers, dns, hourglass, http-client-tls
-, lens, lens-aeson, mtl, optional-args, safe, stdenv
+, bytestring, cassava, containers, dns, hourglass, http-client
+, http-client-tls, http-types, lens, lens-aeson, managed, mtl
+, optional-args, optparse-applicative, resourcet, safe, stdenv
 , system-filepath, text, turtle, unix, unordered-containers
 , utf8-string, vector, yaml
 }:
@@ -12,8 +13,9 @@ mkDerivation {
   isExecutable = true;
   executableHaskellDepends = [
     aeson aeson-pretty amazonka amazonka-s3 base bytestring cassava
-    containers dns hourglass http-client-tls lens lens-aeson mtl
-    optional-args safe system-filepath text turtle unix
+    containers dns hourglass http-client http-client-tls http-types
+    lens lens-aeson managed mtl optional-args optparse-applicative
+    resourcet safe system-filepath text turtle unix
     unordered-containers utf8-string vector yaml
   ];
   license = stdenv.lib.licenses.bsd3;
