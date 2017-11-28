5e3aa54c3bed6235396299186bde7409270be127
Kosyrev Serge <serge.kosyrev@iohk.io>
2017-11-28 18:40:56 +0300

    [DEVOPS-497]  buildkite agents:  separate from Hydra build slaves

--- deployments/infrastructure.nix
+++ deployments/infrastructure.nix
@@ -8,6 +8,12 @@ let
       ./../modules/common.nix
       ./../modules/hydra-slave.nix
     ];
+  };
+  mkBuildkiteAgent = { config, name, pkgs, ... }: {
+    imports = [
+      ./../modules/common.nix
+      ./../modules/hydra-slave.nix
+    ];
     services.buildkite-agent = {
       enable = true;
       name   = name;
@@ -46,6 +52,9 @@ in {
 
   hydra-build-slave-1 = mkHydraBuildSlave;
   hydra-build-slave-2 = mkHydraBuildSlave;
+
+  buildkite-agent-1   = mkBuildkiteAgent;
+  buildkite-agent-2   = mkBuildkiteAgent;
 
   cardano-deployer = { config, pkgs, ... }: {
     imports = [
