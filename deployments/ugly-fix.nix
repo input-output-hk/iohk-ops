8d4559d03d8e0b2ec506595087b5fd281dd3a3b1
michael bishop <michael.bishop@iohk.io>
2017-12-13 15:33:17 -0400

    changes done on the deployer to fix the hydra slaves

---
+++ deployments/ugly-fix.nix
@@ -0,0 +1,20 @@
+{
+  hydra-build-slave-1 = { resources, ... }: {
+    deployment.ec2.keyPair = resources.ec2KeyPairs.cardano-test-eu-central;
+  };
+  hydra-build-slave-2 = { resources, ... }: {
+    deployment.ec2.keyPair = resources.ec2KeyPairs.cardano-keypair-IOHK-eu-central-1;
+  };
+  hydra = { resources, ... }: {
+    deployment.ec2.keyPair = resources.ec2KeyPairs.cardano-test-eu;
+  };
+
+  resources.ec2KeyPairs.cardano-test-eu = {
+    accessKeyId = "cardano-deployer";
+    region = "eu-central-1";
+  };
+  resources.ec2KeyPairs.cardano-test-eu-central = {
+    accessKeyId = "cardano-deployer";
+    region = "eu-central-1";
+  };
+}
