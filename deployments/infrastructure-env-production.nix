7a0517d95d4315a958ea9e48fab6bdde819c0f1b
Kosyrev Serge <serge.kosyrev@iohk.io>
2018-01-25 19:18:22 +0300

    [TEST-ONLY]  infra | datadog:  disable

--- deployments/infrastructure-env-production.nix
+++ deployments/infrastructure-env-production.nix
@@ -10,7 +10,7 @@ in {
 
     imports = [
       ./../modules/papertrail.nix
-      ./../modules/datadog.nix
+      # ./../modules/datadog.nix
     ];
 
     services.dd-agent.tags = ["env:production" "depl:${config.deployment.name}"];
@@ -22,7 +22,7 @@ in {
 
   cardano-deployer = { config, pkgs, resources, ... }: {
     imports = [
-      ./../modules/datadog.nix
+      # ./../modules/datadog.nix
       ./../modules/papertrail.nix
     ];
 
@@ -37,10 +37,10 @@ in {
   };
 
   resources = {
-    datadogMonitors = (with (import ./../modules/datadog-monitors.nix); {
-      disk = mkMonitor disk_monitor;
-      ntp = mkMonitor ntp_monitor;
-    });
+    # datadogMonitors = (with (import ./../modules/datadog-monitors.nix); {
+    #   disk = mkMonitor disk_monitor;
+    #   ntp = mkMonitor ntp_monitor;
+    # });
     elasticIPs = {
       hydra-ip    = { inherit region accessKeyId; };
       cardanod-ip = { inherit region accessKeyId; };
