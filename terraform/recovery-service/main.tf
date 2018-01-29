e0cb7b6fd2a1df520a085fed79844e1a90dce3a9
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2018-01-29 18:01:51 +0000

    [DEVOPS-639] recovery-service: Revert back to local terraform statefile
    Due to the 3rd party support requirements, it would be best to revert
    to managing credentials `terraform.tfvars`.
    
    We also concluded that due to the unique security
    constraints (separate AWS accounts per staging/production) and, again,
    supporting the 3rd party, we won't be using an S3 bucket to manage the
    state file.
    
    Workspaces aren't necessary under this simplified model, assuming the
    staging and production are deployed from different contexts with
    distinct credentials and `terraform.tfvars` files.

--- terraform/recovery-service/main.tf
+++ terraform/recovery-service/main.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.aws_region}"
+}
+
 resource "aws_instance" "recovery_service_web" {
   tags = {
     Name = "Recovery Service Web"
