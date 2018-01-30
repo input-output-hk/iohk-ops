eda00fec1e0584744fbdd93ab064f95b8dc2313e
Kosyrev Serge <serge.kosyrev@iohk.io>
2018-01-30 18:24:50 +0300

    [TEST-ONLY]  Bump nixops to avoid Python-level 'evaluate_only' error

--- default.nix
+++ default.nix
@@ -30,8 +30,8 @@ in rec {
       nixopsUnstable = pkgs.fetchFromGitHub {
         owner = "NixOS";
         repo = "nixops";
-        rev = "c06c0e79ab8d7a58d80b1c38b7ae4ed1a04322f0";
-        sha256 = "1fly6ry7ksj7v5rl27jg5mnxdbjwn40kk47gplyvslpvijk65m4q";
+        rev = "92034401b5291070a93ede030e718bb82b5e6da4";
+        sha256 = "139mmf8ag392w5mn419k7ajp3pgcz6q349n7vm7gsp3g4sck2jjn";
       };
     in (import "${nixopsUnstable}/release.nix" {}).build.${system};
   iohk-ops = pkgs.haskell.lib.overrideCabal
