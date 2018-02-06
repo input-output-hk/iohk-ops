8e8a2d1fa00dd86a884b806fa5c3750de784c10c
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2018-02-06 10:20:34 +0000

    Add bash completion to `io` command
    Uses the built-in support of optparse-applicative. Has two
    limitations:
    
    1. There is no filename completion after --config. To fix this, we
       need to write a variant of `Turtle.Options.optPath` which adds the
       `Options.Applicative.action "filename"` modifier.
    
    2. A single completion takes almost 1 second to run.

--- shell.nix
+++ shell.nix
@@ -8,11 +8,19 @@ let
   ioAlias  = if io-dev-mode
              then ''runhaskell -iiohk iohk/iohk-ops.hs''
              else ''${iohk-ops}/bin/iohk-ops'';
+  ioCompletion = if io-dev-mode then ""
+                 else let iohk = "${iohk-ops}/bin/iohk-ops";
+                 in ''
+                   source <(${iohk} --bash-completion-script ${iohk})
+                   complete -o filenames -F _iohk-ops io
+                 '';
+
 in pkgs.lib.overrideDerivation iohk-ops.env
    (old: {
      shellHook = ''
        function io {
         ${ioAlias} "$@"
        }
+       ${ioCompletion}
      '';
     })
