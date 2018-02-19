a9b28c82f065ac14b4b4fef00311bdb4e55d6cb5
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2018-02-19 08:47:18 +0000

    [DEVOPS-683] Add services.report-server.zendesk options

--- iohk/iohk-ops.hs
+++ iohk/iohk-ops.hs
@@ -321,7 +321,8 @@ runNew o@Options{..} New{..} args = do
     let secrets = [ "static/github_token"
                   , "static/id_buildfarm"
                   , "static/datadog-api.secret"
-                  , "static/datadog-application.secret" ]
+                  , "static/datadog-application.secret"
+                  , "static/zendesk-token.secret" ]
     forM_ secrets touch
     echo "Ensured secrets exist"
 
--- modules/report-server.nix
+++ modules/report-server.nix
@@ -1,6 +1,6 @@
 with (import ./../lib.nix);
 
-params:
+params: environment:
 { pkgs, config, resources, options, ...}:
 
 let
@@ -26,6 +26,32 @@ in {
         type = types.package;
         default = (import ./../default.nix {}).cardano-report-server-static;
       };
+      zendesk = {
+        email = mkOption {
+          type = types.str;
+          # default = "";
+          default = if environment == "Staging"
+            then "report-server-zendesk+staging@iohk.io"
+            else (if environment == "Production"
+                  then "report-server-zendesk+prod@iohk.io"
+                  else "");
+          example = "agent@email.com";
+          description = ''
+            The e-mail associated with the Zendesk account.
+          '';
+        };
+        tokenFile = mkOption {
+          type = types.nullOr types.path;
+          # default = null;
+          default = if environment == "Staging" || environment == "Production"
+            then "/run/keys/zendesk-token"
+            else null;
+          example = "/run/keys/zendesk-token";
+          description = ''
+            An access token for Zendesk.
+          '';
+        };
+      };
     };
   };
 
@@ -49,6 +75,11 @@ in {
 
     deployment.ec2.ebsInitialRootDiskSize = 200;
 
+    deployment.keys.zendesk-token = {
+      keyFile = ./. + "/../static/zendesk-token.secret";
+      user = "report-server";
+    };
+
     networking.firewall.allowedTCPPorts = [
       cfg.port
     ];
@@ -58,6 +89,7 @@ in {
         group = "report-server";
         home = config.services.report-server.logsdir;
         createHome = true;
+        extraGroups = [ "keys" ];
       };
       groups.report-server = {};
     };
@@ -70,10 +102,28 @@ in {
       serviceConfig = {
         User = "report-server";
         Group = "report-server";
-        ExecStart = ''
-          ${cfg.executable}/bin/cardano-report-server -p ${toString cfg.port} --logsdir ${cfg.logsdir}
-        '';
       };
+      script = let
+        zdEmail = if cfg.zendesk.email != "" then "--zd-email \"${cfg.zendesk.email}\"" else "";
+        # fixme: report-server should not accept token as command-line argument
+        zdToken = if cfg.zendesk.tokenFile != null then "--zd-token `head -1 ${cfg.zendesk.tokenFile}`" else "";
+      in ''
+        exec ${cfg.executable}/bin/cardano-report-server \
+            -p ${toString cfg.port} \
+            ${zdEmail} ${zdToken} \
+            --logsdir ${cfg.logsdir}
+      '';
     };
+
+    assertions = [
+      { assertion =
+          (cfg.zendesk.email != "" && cfg.zendesk.tokenFile != null) ||
+          (cfg.zendesk.email == "" && cfg.zendesk.tokenFile == null);
+        message = ''
+          Both `services.report-server.zendesk.email'
+          and `services.report-server.zendesk.tokenFile' must be defined.
+        '';
+      }
+    ];
   };
 }
