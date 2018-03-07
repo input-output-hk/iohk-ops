020d109d366d02f3c30fa455af73200298a06b92
Michael Bishop <michael.bishop@iohk.io>
2018-03-07 19:57:49 -0400

    [DEVOPS-697] add the new benchmarking deployment file

---
+++ deployments/edgenodesv2.nix
@@ -0,0 +1,33 @@
+{ accessKeyId, nodes ? 1 , walletsPerNode ? 1
+, region ? "eu-central-1"
+, topologyFile, systemStart }:
+
+with import ../lib.nix;
+let
+  mkNode = index: {
+    name = "edgenode-${toString index}";
+    value = {
+      imports = [ ../modules/cardano-benchmark.nix ];
+      services.cardano-benchmark.index = index;
+    };
+  };
+in {
+  defaults = { resources, ... }: {
+    services.cardano-benchmark = {
+      inherit systemStart walletsPerNode;
+      topologyFile = "${topologyFile}";
+    };
+    deployment = {
+      targetEnv = "ec2";
+      ec2 = {
+        inherit region accessKeyId;
+        instanceType = mkDefault "t2.large";
+        keyPair = resources.ec2KeyPairs.edgekey;
+        ebsInitialRootDiskSize = 100;
+      };
+    };
+  };
+  resources.ec2KeyPairs.edgekey = {
+    inherit region accessKeyId;
+  };
+} // listToAttrs (map mkNode (range 1 nodes))
---
+++ modules/cardano-benchmark.nix
@@ -0,0 +1,54 @@
+with (import ./../lib.nix);
+
+{ pkgs, config, resources, options, ...}:
+
+let
+  cfg = config.services.cardano-benchmark;
+  iohkpkgs = import ../. {};
+  # relies on the commits cleverca22 made in https://github.com/input-output-hk/cardano-sl/commits/6d83230489ff70985ffd6b7c785286d7535566c4
+  params = index: {
+    topologyFile = cfg.topologyFile;
+    walletListen = "127.0.0.1:${toString (8090 + index)}";
+    ekgListen = "127.0.0.1:${toString (8000 + index)}";
+    environment = "override";
+    confKey = "bench";
+    extraParams = "--system-start ${toString cfg.systemStart}";
+  };
+  mkService = index: {
+    "cardano-node-${toString index}" = {
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        User = "cardano-node-${toString index}";
+        WorkingDirectory = "/home/cardano-node-${toString index}";
+        ExecStart = iohkpkgs.connectScripts.mainnetWallet.override (params index);
+      };
+    };
+  };
+  mkUser = index: {
+    "cardano-node-${toString index}" = {
+      isNormalUser = true;
+    };
+  };
+in {
+  options = {
+    services.cardano-benchmark = {
+      walletsPerNode = mkOption {
+        type = types.int;
+      };
+      systemStart = mkOption {
+        type = types.int;
+      };
+      index = mkOption {
+        type = types.int;
+      };
+      topologyFile = mkOption {
+        type = types.str;
+      };
+    };
+  };
+
+  config = {
+    systemd.services = mkMerge (map mkService (range 1 cfg.walletsPerNode));
+    users.users = mkMerge (map mkUser (range 1 cfg.walletsPerNode));
+  };
+}
