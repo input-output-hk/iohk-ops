8810ec7fc7381e5361aeaf88316f8e1861200959
Michael Bishop <michael.bishop@iohk.io>
2018-03-20 11:48:05 -0300

    [DEVOPS-736] store the zendesk token on the rootfs

--- deployments/report-server-env-staging.nix
+++ deployments/report-server-env-staging.nix
@@ -13,7 +13,7 @@ let nodeMap = { inherit (globals.fullMap) report-server; }; in
     config.services.report-server.zendesk = {
       accountName = "iohksupport";  # NB. expires on 2018-02-26
       email = "staging-report-server@iohk.io";
-      tokenFile = "/run/keys/zendesk-token";
+      tokenFile = "/var/lib/keys/zendesk-token";
     };
   };
 
