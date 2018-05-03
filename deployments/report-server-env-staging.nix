8628ea61f0a3af3661e696cd04ba0a1feef39300
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2018-05-03 09:06:01 +0100

    [DEVOPS-797] staging report-server: use main account

--- deployments/report-server-env-staging.nix
+++ deployments/report-server-env-staging.nix
@@ -11,7 +11,7 @@ let nodeMap = { inherit (globals.fullMap) report-server; }; in
     ];
 
     config.services.report-server.zendesk = {
-      accountName = "iohksupport";  # NB. expires on 2018-02-26
+      accountName = "iohk";
       email = "staging-report-server@iohk.io";
       tokenFile = "/var/lib/keys/zendesk-token";
     };
