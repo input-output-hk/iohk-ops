00ab465f8603b81368d3698da49070f434df74ee
Michael Bishop <cleverca22@gmail.com>
2018-05-15 16:23:46 -0300

    [DEVOPS-841] fix the missing /etc/protocols (#336)

--- modules/nix_nsswitch.nix
+++ modules/nix_nsswitch.nix
@@ -1,23 +1,7 @@
-{ pkgs, lib, ... }:
+{ ... }:
 
-with lib;
 {
-  nixpkgs.overlays = [
-    (self: super: {
-      nix' = super.nix.overrideAttrs ( drv: {
-        # This patch creates a nsswitch.conf in the build environment so cardano-sl
-        # integration tests can pass inside a pure build derivation.
-        patches = [
-          (pkgs.fetchpatch {
-             url = "https://github.com/NixOS/nix/commit/a6c5955c8d59ca26745843f9779253a173876665.patch";
-             name = "nsswitch.patch";
-             sha256 = "1kk2j9bnv3c4ck6c79mwmq04q1qzzq8jvxsi2csyswh9pvxdf1hv";
-           })
-        ];
-      });
-    })
-  ];
   nix = {
-    package = pkgs.nix';
+    sandboxPaths = [ "/etc/nsswitch.conf" "/etc/protocols" ];
   };
 }
