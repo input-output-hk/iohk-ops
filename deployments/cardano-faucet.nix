b7a62a18bd2b6250aa1af15d8e6438727b205ca4
Ben Ford <ben@perurbis.com>
2018-06-04 11:26:17 +0100

    Try and make faucet more like report server

--- deployments/cardano-faucet.nix
+++ deployments/cardano-faucet.nix
@@ -1,18 +1,5 @@
-with (import ./../lib.nix);
 { globals, ... }:
 
 {
-  # faucet = import ./../modules/cardano-faucet.nix globals.fullMap.faucet;
-  faucet = {
-    imports = [
-      ./common.nix
-      ./amazon-base.nix
-      ./network-wide.nix
-      ./../modules/cardano-faucet.nix
-      ./../modules/datadog.nix
-    ];
-
-    global.dnsHostname   = mkForce "faucet";
-
-  };
+  faucet = import ./../modules/cardano-faucet.nix globals.fullMap.faucet;
 }
