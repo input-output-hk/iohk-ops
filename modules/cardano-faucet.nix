ba42d8bbeea5e7f7a31ac85a83514dd413baf4d0
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2018-07-02 20:26:24 +1000

    [DEVOPS-398] faucet: Add recaptcha support to frontend

--- modules/cardano-faucet.nix
+++ modules/cardano-faucet.nix
@@ -3,7 +3,20 @@ with (import ./../lib.nix);
 globals: params:
 { config, pkgs, lib, ...}:
 
-{
+let
+  recaptcha = {
+    siteKey = builtins.readFile ../static/recaptcha-site-key;
+    secretKey = builtins.readFile ../static/recaptcha-secret-key;
+  };
+  explorerURL = "http://cardano-explorer.cardano-testnet.iohkdev.io/";
+  faucetFrontend = pkgs.runCommand "faucet-frontend" {} ''
+    mkdir -p $out
+    sed -e 's|recaptchaSiteKey:.*|recaptchaSiteKey: "${recaptcha.siteKey}",|g' \
+        -e 's|explorerURL:.*|explorerURL: "${explorerURL}",|g' \
+        ${../lib/faucet.html} > $out/index.html
+  '';
+
+in {
   global.dnsHostname = mkForce "cardano-faucet";
 
   imports = [ ./cardano-faucet-module.nix ];
@@ -14,6 +27,7 @@ globals: params:
     configKey = "testnet_full";
     statsd.enable = true;
     statsd.port = 8125; # dogstatsd will be listening on this port
+    inherit recaptcha;
   };
 
   networking.firewall.allowedTCPPorts = [ 80 ];
@@ -28,10 +42,7 @@ globals: params:
         # TLS provided by cloudfront
         locations = {
           "/" = {
-            root = pkgs.runCommand "faucet-frontend" {} ''
-              mkdir -p $out
-              cp ${../lib/faucet.html} $out/index.html
-            '';
+            root = faucetFrontend;
             tryFiles = "$uri $uri/index.html @faucet";
           };
           "@faucet" = {
