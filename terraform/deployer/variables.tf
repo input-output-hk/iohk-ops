e5e178631c88f6097ea8799e82275e12df88e8ff
Kosyrev Serge <serge.kosyrev@iohk.io>
2018-08-03 14:25:44 +0300

    devMantis role

--- terraform/deployer/variables.tf
+++ terraform/deployer/variables.tf
@@ -2,8 +2,9 @@ variable "workspace_iam_roles" {
   description = "AWS subaccount role for deploying resources"
 
   default = {
-    testnet = "arn:aws:iam::117127962627:role/cardano-public-testnet"
-    mainnet = "arn:aws:iam::PRODUCTION-ACCOUNT-ID:role/Terraform"
+    devMantis = "arn:aws:iam::454236594309:role/devMantis-deployer"
+    testnet   = "arn:aws:iam::117127962627:role/cardano-public-testnet"
+    mainnet   = "arn:aws:iam::PRODUCTION-ACCOUNT-ID:role/Terraform"
   }
 }
 
