d5a6a3219584cf6c1fa5928cbdedf37b8bc6a689
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2018-08-10 00:44:43 +1000

    [DEVOPS-398] faucet: Add CORS to NGINX

--- modules/cardano-faucet.nix
+++ modules/cardano-faucet.nix
@@ -44,24 +44,53 @@ in {
     virtualHosts = let
       vhostDomainName = if config.global.dnsDomainname != null
         then config.global.dnsDomainname else "iohkdev.io";
+      # allowOrigin = "https://testnet.iohkdev.io";
+      allowOrigin = "*"; # need to include https://webdevc.iohk.io for the time being
      in {
       "cardano-faucet.${vhostDomainName}" = {
         forceSSL = true;
         enableACME = true;
         locations = {
           "/" = {
-            root = faucetFrontend;
-            tryFiles = "$uri $uri/index.html @faucet";
+            extraConfig = ''
+              if ($request_method = 'OPTIONS') {
+                 add_header 'Access-Control-Allow-Origin' '${allowOrigin}' always;
+                 add_header 'Vary' 'Origin' always;
+                 add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+                 #
+                 # Custom headers and headers various browsers *should* be OK with but aren't
+                 #
+                 add_header 'Access-Control-Allow-Headers' 'DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range';
+                 #
+                 # Tell client that this pre-flight info is valid for 20 days
+                 #
+                 add_header 'Access-Control-Max-Age' 1728000;
+                 add_header 'Content-Type' 'text/plain; charset=utf-8';
+                 add_header 'Content-Length' 0;
+                 return 204;
+              }
+
+              root ${faucetFrontend};
+              try_files $uri $uri/index.html @faucet;
+            '';
           };
           "@faucet" = {
             proxyPass = "http://127.0.0.1:${toString config.services.cardano-faucet.port}";
           };
         };
-        # Otherwise nginx serves files with timestamps unixtime+1 from /nix/store
+        
         extraConfig = ''
+          # Otherwise nginx serves files with timestamps unixtime+1 from /nix/store
           if_modified_since off;
           add_header Last-Modified "";
           etag off;
+
+          # CORS support
+          add_header 'Access-Control-Allow-Origin' '${allowOrigin}' always;
+          add_header 'Vary' 'Origin' always;
+          add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS' always;
+          add_header 'Access-Control-Allow-Headers' 'DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range' always;
+          add_header 'Access-Control-Expose-Headers' 'Content-Length,Content-Range' always;
         '';
       };
     };
