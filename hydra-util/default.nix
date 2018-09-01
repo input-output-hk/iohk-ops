110bf386723b8c4052be661bdd26d9d9d662a4b4
Michael Bishop <michael.bishop@iohk.io>
2018-09-01 15:34:28 -0300

    [DEVOPS-1012] fix build
    make source readable
    strip and shrink-rpath

--- hydra-util/default.nix
+++ hydra-util/default.nix
@@ -8,6 +8,9 @@ let
   ghc = hpkgs.ghcWithPackages (p: with p; [ diagrams diagrams-cairo aeson postgresql-simple containers turtle diagrams-svg formatting servant-server ]);
 in runCommand "hydra-util" { buildInputs = [ ghc hpkgs.ghcid ]; } ''
   cp -vr ${./.}/*.hs .
+  chmod -R +w .
   mkdir -pv $out/bin
   ghc Main.hs -o $out/bin/hydra-util
+  ${binutils-unwrapped}/bin/strip $out/bin/hydra-util
+  patchelf --shrink-rpath $out/bin/hydra-util
 ''
