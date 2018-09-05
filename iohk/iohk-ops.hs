eef5da4cacdb08caf885369eef810294d5feb98e
Samuel Leathers <samuel.leathers@iohk.io>
2018-09-05 17:40:09 -0400

    [DEVOPS-1039] add influxdb/grafana and clean up nginx config for hydra

--- iohk/iohk-ops.hs
+++ iohk/iohk-ops.hs
@@ -314,6 +314,7 @@ runNew o@Options{..} New{..} args = do
     let secrets = [ "static/github_token"
                   , "static/id_buildfarm"
                   , "static/datadog-api.secret"
+                  , "static/google_oauth_hydra_grafana.secret"
                   , "static/datadog-application.secret"
                   , "static/zendesk-token.secret" ]
     forM_ secrets touch
