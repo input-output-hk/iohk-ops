efdb09d21d7ac29e2a19ab581a6625a1e2c490db
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2018-09-29 00:52:23 +1000

    [DEVOPS-779] Remove nix-shell.sh and fetchNixpkgs.nix

--- scripts/set_nixpath.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-update_NIX_PATH() {
-  local scriptDir
-  scriptDir=$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")
-  NIX_PATH="nixpkgs=$(nix-build "${scriptDir}/../fetch-nixpkgs.nix" -o nixpkgs)"
-  export NIX_PATH
-}
-update_NIX_PATH
