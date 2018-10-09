b9e0fc85ce0dee02cd3b29e8a84e611a56ab59c3
Kosyrev Serge <serge.kosyrev@iohk.io>
2018-10-09 16:35:13 +0300

    [DEVOPS-1047]  automate packet.net machine enrollment somewhat

---
+++ scripts/enroll-packetnet-machine.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+machine_name=$1
+
+test -n "$machine_name" || {
+        echo "Usage: $(basename $0) MACHINE-NAME" >&2
+        exit 1
+}
+test ! -d deployments/${machine_name} || {
+        echo "FATAL: deployments/${machine_name} already exists" >&2
+        exit 1
+}
+
+scp -r -o StrictHostKeyChecking=accept-new root@${machine_name}.aws.iohkdev.io:/etc/nixos/packet/ deployments/${machine_name}
