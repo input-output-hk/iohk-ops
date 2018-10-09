c7ef2196a8ef09b239b96dd69f2e97d2f26ab1b9
Kosyrev Serge <serge.kosyrev@iohk.io>
2018-10-09 16:57:41 +0300

    [DEVOPS-1047]  Two slaves for Mantis Hydra: mantis-slave-packet-{1,2}

---
+++ deployments/mantis-slave-packet-1/host-id.nix
@@ -0,0 +1,1 @@
+{ networking.hostId = "0e8388eb"; }
--- deployments/packet-net.nix
+++ deployments/packet-net.nix
@@ -35,6 +35,9 @@ in
   builder-packet-c1-small-x86-4 = mkPacketNet "builder-packet-c1-small-x86-4" ../modules/hydra-slave.nix;
   builder-packet-c1-small-x86-5 = mkPacketNet "builder-packet-c1-small-x86-5" ../modules/hydra-slave.nix;
 
+  mantis-slave-packet-1         = mkPacketNet "mantis-slave-packet-1"         ../modules/hydra-slave.nix;
+  mantis-slave-packet-2         = mkPacketNet "mantis-slave-packet-2"         ../modules/hydra-slave.nix;
+
   buildkite-packet-1            = mkPacketNet "buildkite-packet-1"            ../modules/buildkite-agent.nix;
   buildkite-packet-2            = mkPacketNet "buildkite-packet-2"            ../modules/buildkite-agent.nix;
   buildkite-packet-3            = mkPacketNet "buildkite-packet-3"            ../modules/buildkite-agent.nix;
