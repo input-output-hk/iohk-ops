7f6f4bb3ee8c72e712e1f05490006dc58aa28f9d
Kosyrev Serge <serge.kosyrev@iohk.io>
2018-10-10 16:55:23 +0300

    [DEVOPS-1047]  CI: enable packet.net component for all infra deployments

--- iohk/common/Constants.hs
+++ iohk/common/Constants.hs
@@ -85,6 +85,7 @@ envSettings env =
                                , (Faucet,         All, "deployments/cardano-faucet.nix")
                                , (Nodes,          All, "deployments/cardano-nodes.nix")
                                , (Infra,          All, "deployments/infrastructure.nix")
+                               , (Infra,          All, "deployments/packet-net.nix")
                                , (Infra,          AWS, "deployments/infrastructure-target-aws.nix") ]
   in case env of
     Staging      -> EnvSettings
@@ -111,7 +112,6 @@ envSettings env =
                                , (Nodes,          All, "deployments/cardano-nodes-env-production.nix")
                                , (Explorer,       All, "deployments/cardano-explorer-env-production.nix")
                                , (ReportServer,   All, "deployments/report-server-env-production.nix")
-                               , (Infra,          All, "deployments/packet-net.nix")
                                , (Infra,          All, "deployments/infrastructure-env-production.nix")
                                ] <> deplAgnosticFiles}
     Testnet  -> EnvSettings
