6d040403e9a4ae64e49b1f01474fd0ac85ddedca
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2018-10-23 23:36:57 +1000

    infra: Provide minimal build environment for buildkite-agent
    Provide git-lfs in the Buildkite environment so that LFS repos can be
    cloned from GitHub.
    
    Set the NIX_PATH for the builds to be what's running on the
    buildkite-agent system. Mostly, our builds will be pinning the nixpkgs
    version, so this is usually irrelevant. But sometimes it's helpful to
    be able to use a nix package from the host system's package set. For
    example, docker.

--- modules/buildkite-agent.nix
+++ modules/buildkite-agent.nix
@@ -14,10 +14,16 @@
     openssh.publicKeyPath  = "/run/keys/buildkite-ssh-public";
     tokenPath              = "/run/keys/buildkite-token";
     meta-data              = "system=x86_64-linux";
-    runtimePackages        = with pkgs; [ bash gnutar gzip bzip2 xz nix ];
+    runtimePackages        = with pkgs; [
+       bash gnutar gzip bzip2 xz
+       git git-lfs
+       nix
+    ];
     hooks.environment = ''
+      # Provide a minimal build environment
       export NIX_BUILD_SHELL="/run/current-system/sw/bin/bash"
       export PATH="/run/current-system/sw/bin:$PATH"
+      export NIX_PATH="nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos"
 
       # load S3 credentials for artifact upload
       source /var/lib/buildkite-agent/hooks/aws-creds
