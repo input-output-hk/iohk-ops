8339cbc5dad765d7d706e8fc8df01f21214d52a6
Michael Bishop <michael.bishop@iohk.io>
2018-11-24 18:33:51 -0400

    add the firewall rules

--- deployments/voice-server.nix
+++ deployments/voice-server.nix
@@ -4,11 +4,14 @@ let
 in {
   voice-server = { config, resources, ... }: {
     deployment = {
+      targetEnv = "ec2";
       ec2 = {
         inherit region accessKeyId;
-        keyPair = resources.ec2KeyPairs.voice-server;
-        instanceType = "t3.nano";
+        keyPair = resources.ec2KeyPairs.voice-server-key;
+        instanceType = "t3.micro";
+        securityGroups = [ resources.ec2SecurityGroups.voice-server-firewall ];
       };
+      route53.hostName = "voice-server.aws.iohkdev.io";
     };
     services = {
       teamspeak3 = {
@@ -23,7 +26,16 @@ in {
       };
     };
   };
-  resources.ec2KeyPairs.voice-server = {
+  resources.ec2KeyPairs.voice-server-key = {
     inherit region accessKeyId;
+  };
+  resources.ec2SecurityGroups.voice-server-firewall = {
+    inherit region accessKeyId;
+    rules = [
+      { protocol = "tcp"; fromPort = 22; toPort = 22; sourceIp = "0.0.0.0/0"; }
+      { protocol = "tcp"; fromPort = 30033; toPort = 30033; sourceIp = "0.0.0.0/0"; }
+      { protocol = "tcp"; fromPort = 10011; toPort = 10011; sourceIp = "0.0.0.0/0"; }
+      { protocol = "udp"; fromPort = 9987; toPort = 9987; sourceIp = "0.0.0.0/0"; }
+    ];
   };
 }
