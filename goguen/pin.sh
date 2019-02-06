04c2b22dc53f354d76e2f45b20b8610edeaeeb95
Kosyrev Serge <serge.kosyrev@iohk.io>
2019-02-06 19:07:01 +0300

    Goguen pins:  submodules!

--- goguen/pin.sh
+++ goguen/pin.sh
@@ -5,7 +5,7 @@ nhroot="$(realpath $0 | xargs dirname)"
 cwdRepo="$(basename $(pwd))"
 
 user=$1
-test -n "${user}" || { echo "USAGE: $(basename $0) GITHHUB-USER GITHUB-REPO COMMIT-ID" >&2; exit 1; }
+test -n "${user}" || { echo "USAGE: $(basename $0) COMMIT-ID [GITHUB-USERNAME]" >&2; exit 1; }
 
 repo=${2:-${cwdRepo}}
 rev=${3:-$(if test "${cwdRepo}" = "${repo}"; then git rev-parse HEAD; fi)}
@@ -15,4 +15,5 @@ case ${user} in
     local | home ) base=file://$HOME; user='';;
     * )            base=https://github.com/ ;;
 esac
-nix-prefetch-git ${base}${user}/${repo} ${rev} | tee "${nhroot}"/pins/${repo}.src-json
+nix-prefetch-git --fetch-submodules ${base}${user}/${repo} ${rev} | tee "${nhroot}"/pins/${repo}.src-json
+
