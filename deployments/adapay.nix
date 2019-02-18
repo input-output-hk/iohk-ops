4e5ad89955b99d7345eb2b00b05a6a00a6222b35
Samuel Leathers <samuel.leathers@iohk.io>
2019-02-18 17:53:03 -0500

    more reverse proxy changes

--- deployments/adapay.nix
+++ deployments/adapay.nix
@@ -175,7 +175,7 @@
             } // lib.optionalAttrs (esConfig != {}) {
               "/kibana/".extraConfig = ''
                 proxy_pass ${esConfig.kibanaUrl};
-                proxy_redirect ${esConfig.kibanaUrl} https://monitoring.${environment}.adapay.iohk.io/kibana/;
+                proxy_redirect ${esConfig.kibanaUrl} https://monitoring.${environment}.adapay.iohk.io/;
                 proxy_set_header Host $http_host;
                 proxy_set_header REMOTE_ADDR $remote_addr;
                 proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
