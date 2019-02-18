fc5d6fc80d270ae0672a87a9e1dc866511435f9b
Samuel Leathers <samuel.leathers@iohk.io>
2019-02-18 11:12:57 -0500

    kibana changes

--- deployments/adapay.nix
+++ deployments/adapay.nix
@@ -174,7 +174,7 @@
               '';
             } // lib.optionalAttrs (esConfig != {}) {
               "/kibana/".extraConfig = ''
-                proxy_pass http://${esConfig.kibana_url}/;
+                proxy_pass http://${esConfig.kibanaUrl}/;
                 proxy_set_header Host $http_host;
                 proxy_set_header REMOTE_ADDR $remote_addr;
                 proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
