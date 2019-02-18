b2de792a66f58aa6d078b55de3946dcbdd0d2250
Samuel Leathers <samuel.leathers@iohk.io>
2019-02-18 01:56:47 -0500

    sync production aws config with staging

--- deployments/adapay-aws-production.nix
+++ deployments/adapay-aws-production.nix
@@ -21,7 +21,6 @@ in {
           resources.ec2SecurityGroups.adapaySG.name
           resources.ec2SecurityGroups.adapaySGimporter.name
         ];
-        tags = { inherit environment; };
       };
       targetEnv = "ec2";
     };
@@ -44,7 +43,6 @@ in {
           resources.ec2SecurityGroups.adapaySG.name
           resources.ec2SecurityGroups.adapaySGadapay.name
         ];
-        tags = { inherit environment; };
       };
       targetEnv = "ec2";
     };
@@ -68,7 +66,6 @@ in {
           resources.ec2SecurityGroups.adapaySG.name
           resources.ec2SecurityGroups.adapaySGnginx.name
         ];
-        tags = { inherit environment; };
       };
       targetEnv = "ec2";
       route53 = {
@@ -80,15 +77,39 @@ in {
     # Don't reconfigure the system from EC2 userdata on next startup
     systemd.services.amazon-init.wantedBy = lib.mkForce [ ];
   };
+  monitoring = { config, pkgs, lib, resources, ... }: {
+    ec2.hvm = true;
+
+    deployment = {
+      ec2 = {
+        inherit accessKeyId region zone;
+        ebsInitialRootDiskSize = 200;
+        instanceType = "t2.medium";
+        keyPair = resources.ec2KeyPairs.adapayKey;
+        subnetId = resources.vpcSubnets.adapayVPCSubnet;
+        associatePublicIpAddress = true;
+        securityGroupIds = [
+          resources.ec2SecurityGroups.adapaySG.name
+          resources.ec2SecurityGroups.adapaySGmonitor.name
+        ];
+      };
+      targetEnv = "ec2";
+      route53 = {
+        inherit accessKeyId;
+        hostName = "monitoring.${environment}.adapay.iohk.io";
+      };
+    };
+
+    # Don't reconfigure the system from EC2 userdata on next startup
+    systemd.services.amazon-init.wantedBy = lib.mkForce [ ];
+  };
   resources = {
     elasticIPs.adapayIP = {
       inherit accessKeyId region;
       vpc = true;
-      tags = { inherit environment; };
     };
     ec2KeyPairs.adapayKey = {
       inherit accessKeyId region;
-      tags = { inherit environment; };
     };
     ec2SecurityGroups = let
       allowPortSource = source: port: {
@@ -102,58 +123,55 @@ in {
       adapaySG = { resources, ... }: {
         inherit accessKeyId region;
         vpcId = resources.vpc.adapayVPC;
-        rules = map allowPortPublic [ 22 ];
-        tags = { inherit environment; };
+        rules = (map allowPortPublic [ 22 ]) ++ (map allowPortVPC [ 9100 ]);
       };
       adapaySGnginx = { resources, ... }: {
         inherit accessKeyId region;
         vpcId = resources.vpc.adapayVPC;
+        rules = (map allowPortPublic [ 80 443 ]) ++
+                (map allowPortVPC [ 9113 ]);
+      };
+      adapaySGmonitor = { resources, ... }: {
+        inherit accessKeyId region;
+        vpcId = resources.vpc.adapayVPC;
         rules = map allowPortPublic [ 80 443 ];
-        tags = { inherit environment; };
       };
       adapaySGimporter = { resources, ... }: {
         inherit accessKeyId region;
         vpcId = resources.vpc.adapayVPC;
         rules = [ (allowPortVPC 8200) ];
-        tags = { inherit environment; };
       };
       adapaySGadapay = { resources, ... }: {
         inherit accessKeyId region;
         vpcId = resources.vpc.adapayVPC;
-        rules = [ (allowPortVPC 8081) ];
-        tags = { inherit environment; };
+        rules = []; # (allowPortVPC 8081) ];
       };
     };
     vpc.adapayVPC = {
       inherit accessKeyId region;
       enableDnsSupport = true;
       cidrBlock = "10.0.0.0/16";
-      tags = { inherit environment; };
     };
     vpcSubnets.adapayVPCSubnet = { resources, ... }: {
       inherit accessKeyId region zone;
       cidrBlock = "10.0.1.0/24";
       vpcId = resources.vpc.adapayVPC;
       mapPublicIpOnLaunch = true;
-      tags = { inherit environment; };
     };
     vpcInternetGateways.adapayIGW = { resources, ... }: {
       inherit region accessKeyId;
       vpcId = resources.vpc.adapayVPC;
-      tags = { inherit environment; };
     };
 
     vpcRouteTables.adapayRouteTable =  { resources, ... }: {
       inherit region accessKeyId;
       vpcId = resources.vpc.adapayVPC;
-      tags = { inherit environment; };
     };
 
     vpcRouteTableAssociations.adapayAssociation = { resources, ... }: {
       inherit region accessKeyId;
       subnetId = resources.vpcSubnets.adapayVPCSubnet;
       routeTableId = resources.vpcRouteTables.adapayRouteTable;
-      tags = { inherit environment; };
     };
 
     vpcRoutes.adapayIGWRoute = { resources, ... }: {
@@ -161,7 +179,6 @@ in {
       routeTableId = resources.vpcRouteTables.adapayRouteTable;
       destinationCidrBlock = "0.0.0.0/0";
       gatewayId = resources.vpcInternetGateways.adapayIGW;
-      tags = { inherit environment; };
     };
   };
 }
