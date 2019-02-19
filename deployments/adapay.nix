b426d4bb1adb15db1b36a8573933af85b21b930b
Samuel Leathers <samuel.leathers@iohk.io>
2019-02-18 21:00:43 -0500

    kibana nginx config

--- deployments/adapay.nix
+++ deployments/adapay.nix
@@ -206,7 +206,7 @@
                 proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
                 proxy_set_header X-Forwarded-Proto https;
               '';
-              "/kibana".extraConfig = ''
+              "/kibana/".extraConfig = ''
                 ${oauthProxyConfig}
                 proxy_pass http://127.0.0.1:5601/;
                 proxy_http_version 1.1;
