6000985eb3805b032c17749799be47e1bc58eac4
Kosyrev Serge <serge.kosyrev@iohk.io>
2019-03-28 15:35:52 +0300

    Goguen-ala-Cardano:  non-invasive definitions

---
+++ modules/configurator.nix
@@ -0,0 +1,28 @@
+{ lib, config, ... }: with lib; with types; let
+  cfg = config.configurator;
+in {
+
+  imports = [ <module/parameters.nix> <module/parameters-ops.nix> ];
+
+  options.configurator = {
+    optionDefaults = mkOption {
+      description = "Option defaults";
+      default = {};
+      type = attrs;
+    };
+    userConfig = mkOption {
+      description = "Cluster configuration";
+      default = {};
+      type = attrs;
+    };
+  };
+
+  config = let
+    optionDefaults = mapAttrsRecursive (_: mkOverride 1250) cfg.optionDefaults;
+    userConfig     = mapAttrsRecursive (_: mkForce)         cfg.userConfig;
+    mergedConfig   = builtins.deepSeq [optionDefaults userConfig ] recursiveUpdate optionDefaults userConfig;
+  in {
+      node = mergedConfig.node;
+      cluster = mergedConfig.cluster;
+  };
+}
