04e812d5343cc5b7957f2683d109176c7849795b
Jean-Baptiste Giraudeau <jean-baptiste.giraudeau@iohk.io>
2019-04-08 17:05:42 +0200

    Add chain quality rule for cardano.

--- deployments/monitoring-env-staging.nix
+++ deployments/monitoring-env-staging.nix
@@ -6,5 +6,19 @@
       ../modules/staging.nix
     ];
     services.monitoring-services.applicationDashboards = ../modules/grafana/cardano;
+    services.monitoring-services.applicationRules = [
+      {
+        alert = "chain_quality_degraded";
+        expr = "cardano_chain_quality_last_k__2160__blocks__ < 99";
+        for = "5m";
+        labels = {
+          severity = "page";
+        };
+        annotations = {
+          summary = "{{$labels.alias}}: Chain quality last 2160 blocks degraded.";
+          description = "{{$labels.alias}}: Chain quality last 2160 blocks degraded (<99%).";
+        };
+      }
+    ];
   };
 }
