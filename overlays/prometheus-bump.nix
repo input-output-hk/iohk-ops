2983dfd1e880c962016772092efcd73a1ac8e53c
Jean-Baptiste Giraudeau <jean-baptiste.giraudeau@iohk.io>
2019-04-09 19:20:13 +0200

    renamed function: generic -> buildPrometheus

--- overlays/prometheus-bump.nix
+++ overlays/prometheus-bump.nix
@@ -1,5 +1,5 @@
 self: super: {
-  prometheus_2 = (super.callPackage <nixpkgs/pkgs/servers/monitoring/prometheus> {}).generic {
+  prometheus_2 = (super.callPackage <nixpkgs/pkgs/servers/monitoring/prometheus> {}).buildPrometheus {
     version = "2.8.1";
     sha256 = "0x8w0qdh4lcf19nmdlhvgzpy08c2a932d3k49cjwhi5npcsf858n";
     doCheck = false;
