1c05ed29d177ee84146d92629c8467577f79b037
Jean-Baptiste Giraudeau <jean-baptiste.giraudeau@iohk.io>
2019-04-11 11:42:55 +0200

    Allow up to 1ms offset vs NTP time.
     instead of 0.1ms that trigger too much
     false positive.

--- modules/monitoring-services.nix
+++ modules/monitoring-services.nix
@@ -475,7 +475,7 @@ in {
                     }
                     {
                       alert = "node_time_unsync";
-                      expr = "abs(node_timex_offset_seconds) > 0.0001 or node_timex_sync_status != 1";
+                      expr = "abs(node_timex_offset_seconds) > 0.001 or node_timex_sync_status != 1";
                       for = "1m";
                       labels = {
                         severity = "page";
