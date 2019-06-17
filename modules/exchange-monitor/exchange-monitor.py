f575300258e4393ed92eb68063be2e4af4668b64
neville-freeman <42548310+neville-freeman@users.noreply.github.com>
2019-06-17 13:03:03 +0100

    Adding monitoring for bithumb

--- modules/exchange-monitor/exchange-monitor.py
+++ modules/exchange-monitor/exchange-monitor.py
@@ -13,10 +13,12 @@ EXPORTER_PORT = 8000
 SLEEP_TIME = 300 # 5 minutes
 BINANCE_REQUEST_TIME = Summary('binance_process_time', 'Time spent processing binance assets')
 BITTREX_REQUEST_TIME = Summary('bittrex_process_time', 'Time spent processing bittrex assets')
+BITHUMB_REQUEST_TIME = Summary('bithumb_process_time', 'Time spent processing bithumb assets')
 binance_deposits = Gauge('binance_deposits', 'Binance Deposits enabled')
 binance_withdraws = Gauge('binance_withdraws', 'Binance Withdraws enabled')
 bittrex_active = Gauge('bittrex_active', 'Bittrex Wallet enabled')
 bittrex_withdraw_queue_depth = Gauge('bittrex_withdraw_queue_depth', 'Bittrex Withdraw Queue Depth')
+bithumb_active = Gauge('bithumb_active', 'Bithumb Wallet enabled')
 
 # Decorate function with metric.
 @BINANCE_REQUEST_TIME.time()
@@ -38,11 +40,22 @@ def process_bittrex_assets():
     json_obj = urllib.request.urlopen(url)
     crypto_assets = json.loads(json_obj.read().decode('utf-8'))["result"]
     print("processing bittrex assets")
-
     for crypto_asset in crypto_assets:
         if crypto_asset['Health']['Currency'] == "ADA":
             bittrex_active.set(crypto_asset['Health']['IsActive'])
             bittrex_withdraw_queue_depth.set(crypto_asset['Health']['WithdrawQueueDepth'])
+    sys.stdout.flush()
+
+        # Decorate function with metric.
+@BITHUMB_REQUEST_TIME.time()
+def process_bithumb_assets():
+    url = "https://api.bithumb.com/public/ticker/ADA"
+    json_obj = urllib.request.urlopen(url)
+    crypto_assets = json.loads(json_obj.read().decode('utf-8'))
+    print("processing bithumb assets")
+    for crypto_asset in crypto_assets:
+        if crypto_asset['status'] == '0000':
+        bithumb_active.set(crypto_asset['Health']['IsActive'])
     sys.stdout.flush()
 
 if __name__ == '__main__':
@@ -62,4 +75,9 @@ if __name__ == '__main__':
             print("failed to process bittrex assets")
             bittrex_active.set(False)
             bittrex_withdraw_queue_depth.set(False)
+        try:
+            process_bithumb_assets()
+         except:
+            print("failed to process bithumb assets")
+            bithumb_active.set(False)
         time.sleep(SLEEP_TIME)
