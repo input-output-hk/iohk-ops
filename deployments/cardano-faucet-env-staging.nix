4a3a353725b81890368541cd96d1d05da3ba411e
Samuel Leathers <samuel.leathers@iohk.io>
2019-08-08 10:18:57 -0400

    faucet fix for staging

---
+++ deployments/cardano-faucet-env-staging.nix
@@ -0,0 +1,24 @@
+{ globals, ... }: with (import ../lib.nix);
+let nodeMap = { inherit (globals.fullMap) faucet; }; in
+
+{
+  faucet = (import ../modules/cardano-staging.nix) nodeMap.faucet;
+  monitoring = {
+    services.monitoring-services = {
+      applicationRules = [
+        {
+          alert = "cardano_faucet_balance_monitor";
+          expr = "faucet_wallet_balance < 10000000000";
+          for = "1m";
+          labels.severity = "page";
+          annotations = {
+            description = "{{$labels.alias}} Staging faucet wallet balance is low";
+          };
+        }
+      ];
+    };
+  };
+  resources = {
+    elasticIPs = nodesElasticIPs nodeMap;
+  };
+}
