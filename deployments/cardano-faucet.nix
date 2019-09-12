bf2502c4756f890bd70d013d1b9a9b2cca5132f5
Michael Fellinger <michael.fellinger@iohk.io>
2019-09-12 15:47:38 +0200

    Add jormungandr faucet

--- deployments/cardano-faucet.nix
+++ deployments/cardano-faucet.nix
@@ -1,9 +1,15 @@
-{ globals, ... }: with (import ../lib.nix);
+{ globals, config, ... }: with (import ../lib.nix);
 let nodeMap = { inherit (globals.fullMap) faucet; }; in
 
 {
-  faucet = {
+  faucet = if config.params.nodeImpl == "haskell" then {
     imports = [ ../modules/cardano-base.nix ../modules/cardano-faucet.nix ];
     params = nodeMap.faucet;
-  };
+  } else
+    if config.params.nodeImpl == "rust" then {
+    imports = [
+      (sources.jormungandr-nix + "/nixos")
+      (sources.jormungandr-faucet + "/nix/nixos")
+    ];
+  } else {};
 }
