9ba27507364c65c0c9323ead7060d5ce335eba4f
John Lotoski <john.lotoski@iohk.io>
2019-09-17 15:16:32 -0400

    Add buildkite artifact creds to ci role

--- deployments/mac-base.nix
+++ deployments/mac-base.nix
@@ -25,6 +25,10 @@ let
           destDir = "/var/lib/macos-vm-persistent-config-signing/buildkite";
           keyFile = ../static/buildkite_token;
         };
+        "buildkite_aws_creds_ci" = {
+          destDir = "/var/lib/macos-vm-persistent-config-ci/buildkite";
+          keyFile = ../static/buildkite-hook;
+        };
         "buildkite_aws_creds_signing" = {
           destDir = "/var/lib/macos-vm-persistent-config-signing/buildkite";
           keyFile = ../static/buildkite-hook;
