790a5b10226094858910f72a4cc57736d5ff9955
John Lotoski <john.lotoski@iohk.io>
2019-09-19 19:51:14 -0400

    Adds container common module and customization opt

--- modules/buildkite-agent-container.nix
+++ modules/buildkite-agent-container.nix
@@ -6,30 +6,53 @@ let
 
 in with lib;
 {
+  imports = [
+    ./auto-gc.nix
+  ];
+
   options = {
     services.buildkite-containers = {
       hostIdSuffix = mkOption {
         type = types.str;
         default = "1";
         description = ''
-          A host identifier suffix which is typically a CI server number and is used
-          as part of the container name.  Container names are limited to 7 characters,
-          so the default naming convention is ci${hostIdSuffix}-<containerNum>.
-          An example container name, using a hostIdSuffix of 2 for example, may then
-          be ci2-4, indicating a 4th CI container on a 2nd host CI server.
+          A host identifier suffix which, is typically a CI server number, is used
+          as part of the container name when the default container list is deployed.
+          Container names are limited to 7 characters, so the default naming convention
+          is ci${hostIdSuffix}-<containerNum>.  An example container name, using a
+          hostIdSuffix of 2 for example, may then be ci2-4, indicating a 4th CI
+          container on a 2nd host CI server.
         '';
         example = "1";
+      };
+
+      containerList = mkOption {
+        type = types.listOf types.attrs;
+        default = [ ];
+        description = ''
+          This parameter allows container customization on a per server basis.  If
+          left as a default empty list, a predefined and fixed deployment of
+          containers will pushed to each host which includes this module.  If this
+          option is provided a list of attributes, each representing a Buildkite
+          container, this list of containers will be used instead of the predefined
+          list.  See the buildkite-agent-container.nix module for possible customizations.
+          Note that container names cannot be more than 7 characters.
+        '';
+        example = ''
+          [ { containerName = "ci1-1"; ipo4 = "10"; metadata = "system=x86_64-linux,queue=custom"; }
+            { containerName = "ci1-2"; ipo4 = "11"; metadata = "system=x86_64-linux,queue=custom"; } ];
+        '';
       };
     };
   };
 
   config = let
-    createBuildkiteContainer = { containerName
-                               , baseVethIp ? "10.254.1"
-                               , hostIpo4 ? "1"
-                               , ipo4 ? "10"
-                               , metadata ? "system=x86_64-linux,queue=test"
-                               }: {
+    createBuildkiteContainer = { containerName                                 # The desired container name
+                               , baseVethIp ? "10.254.1"                       # The first 3 octets of the IPv4 host and guest virtual eth nic IP
+                               , hostIpo4 ? "1"                                # The fourth octet of the IPv4 host virtual eth nic IP
+                               , ipo4 ? "10"                                   # The fourth octet of the IPv4 container guest virtual eth nic IP
+                               , metadata ? "system=x86_64-linux,queue=test"   # Agent metadata customization
+                              }: {
       name = containerName;
       value = {
         autoStart = true;
@@ -46,14 +69,15 @@ in with lib;
         localAddress = baseVethIp + "." + ipo4;
         config = {
           imports = [
-            ./auto-gc.nix
             ./nix_nsswitch.nix
             ./docker-builder.nix
+            ./common.nix
           ];
-
+          services.monitoring-exporters.enable = false;
+          services.ntp.enable = mkForce false;
           services.buildkite-agent = {
             enable = true;
-            name   = name;
+            name   = name + "-" + containerName;
             openssh.privateKeyPath = "/run/keys/buildkite-ssh-private";
             openssh.publicKeyPath  = "/run/keys/buildkite-ssh-public";
             tokenPath              = "/run/keys/buildkite-token";
@@ -185,12 +209,12 @@ in with lib;
     networking.nat.externalInterface = "bond0";
 
     containers = let
-      buildkiteContainerList = [
+      buildkiteContainerList = if (length cfg.containerList == 0) then ([
         { containerName = "ci${cfg.hostIdSuffix}-1"; ipo4 = "10"; }
         { containerName = "ci${cfg.hostIdSuffix}-2"; ipo4 = "11"; }
-        { containerName = "ci${cfg.hostIdSuffix}-3"; ipo4 = "12"; metadata = "system=x86_64-linux,queue=daedalusTest"; }
-        { containerName = "ci${cfg.hostIdSuffix}-4"; ipo4 = "13"; metadata = "system=x86_64-linux,queue=daedalusTest"; }
-      ];
+        { containerName = "ci${cfg.hostIdSuffix}-3"; ipo4 = "12"; }
+        { containerName = "ci${cfg.hostIdSuffix}-4"; ipo4 = "13"; }
+      ]) else cfg.containerList;
     in
       builtins.listToAttrs (map createBuildkiteContainer buildkiteContainerList);
   };
