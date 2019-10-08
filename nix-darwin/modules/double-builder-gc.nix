1ee038b61fc802e54287d8acc45cec8fdcc5f208
John Lotoski <john.lotoski@iohk.io>
2019-10-08 10:42:39 -0400

    Optimized mac-mini CI * Optimized auto and launchDaemon GC * Disables screensaver * Creates /scratch and /cache folders to match linux bk agents

--- nix-darwin/modules/double-builder-gc.nix
+++ nix-darwin/modules/double-builder-gc.nix
@@ -1,19 +1,21 @@
 { config, lib, pkgs, ... }:
 
 let
-  maxFreedMB = 25000;
-  minFreeMB = 6000;
+  nixAutoMaxFreedMB = 33000;     # An absolute amount to free
+  nixAutoMinFreeMB = 10000;
+  maxFreedMB = 25000;            # A relative amount to free
+  minFreeMB = 8000;
 
 in {
   imports = [ ../services/builder-gc.nix ];
 
   # This GC is run automatically by nix-build
   nix.extraOptions = ''
-    # Try to ensure between ${toString minFreeMB}M and ${toString maxFreedMB}M of free space by
+    # Try to ensure between ${toString nixAutoMinFreeMB}M and ${toString nixAutoMaxFreedMB}M of free space by
     # automatically triggering a garbage collection if free
     # disk space drops below a certain level during a build.
-    min-free = ${toString (minFreeMB * 1048576)}
-    max-free = ${toString (maxFreedMB * 1048576)}
+    min-free = ${toString (nixAutoMinFreeMB * 1048576)}
+    max-free = ${toString (nixAutoMaxFreedMB * 1048576)}
   '';
 
   # This GC is run on 15 minute intervals
