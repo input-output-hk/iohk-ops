c02dd311840e9879253111200ff5cab298285bce
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2019-11-19 14:42:14 +1000

    buildkite-agent: provide docker credentials

--- modules/docker-builder.nix
+++ modules/docker-builder.nix
@@ -7,4 +7,25 @@
     autoPrune.dates = "daily";
     autoPrune.flags = [ "--all" "--force" ];
   };
+
+  # Provide dockerhub credentials to buildkite
+  systemd.services.buildkite-agent-setup-docker = {
+    wantedBy = [ "buildkite-agent.service" ];
+    script = ''
+      mkdir -p ~buildkite-agent/.docker
+      ln -sf /run/keys/dockerhub-auth ~buildkite-agent/.docker/config.json
+      chown -R buildkite-agent:nogroup ~buildkite-agent/.docker
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+    };
+  };
+
+  # DockerHub password/token (base64-encoded in json)
+  deployment.keys = {
+    dockerhub-auth = {
+      keyFile = ./. + "/../static/dockerhub-auth-config.json";
+      user    = "buildkite-agent";
+    };
+  };
 }
