967430c8c000310d7ef2a863801007ffad43ab0c
Craige McWhirter <craige.mcwhirter@iohk.io>
2019-11-20 14:28:57 +1000

    Fine tuning garbage collection configuration

--- modules/auto-gc.nix
+++ modules/auto-gc.nix
@@ -1,11 +1,24 @@
 { pkgs, ... }:
-
-{
+let
+  nixAutoMaxFreedMB = 20000;     # An absolute amount to free
+  nixAutoMinFreeMB  = 10000;     # The min to trigger a GC at
+in {
   nix = {
     gc = {
       automatic = true;
       dates = "*:15:00";
-      options = ''--max-freed "$((64 * 1024**3 - 1024 * $(df -P -k /nix/store | tail -n 1 | ${pkgs.gawk}/bin/awk '{ print $4 }')))"'';
+
+      # Set the max absolute level to free to 30 GB on the /nix/store mount
+      options = ''--max-freed "$((30 * 1024**3 - 1024 * $(df -P -k /nix/store | tail -n 1 | ${pkgs.gawk}/bin/awk '{ print $4 }')))"'';
     };
+
+    # This GC is run automatically by nix-build
+    extraOptions = ''
+      # Try to ensure between ${toString nixAutoMinFreeMB}M and ${toString nixAutoMaxFreedMB}M of free space by
+      # automatically triggering a garbage collection if free
+      # disk space drops below a certain level during a build.
+      min-free = ${toString (nixAutoMinFreeMB * 1048576)}
+      max-free = ${toString (nixAutoMaxFreedMB * 1048576)}
+    '';
   };
 }
