baf627f7209d18b584604c82ba093b3de130514f
Michael Bishop <michael.bishop@iohk.io>
2020-01-22 12:23:26 -0400

    fix declarative jobsets

--- jobsets/default.nix
+++ jobsets/default.nix
@@ -296,7 +296,7 @@ let
       nixexprpath = "release.nix";
       inputs = {
         "${input}" = mkFetchGithub "${info.base.repo.clone_url} pull/${num}/head";
-        pr = { type = "string"; value = num; };
+        pr = { type = "string"; value = num; emailresponsible = false; };
       };
     };
   };
